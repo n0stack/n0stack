ae5ead8fefa9a8061671f5fd6120e083f62a3e65
palloc <palloc.mknet10@gmail.com>
2017-09-22 14:25:55 +0900

    add json parser

--- src/resource/vm.py
+++ src/resource/vm.py
@@ -43,4 +43,38 @@ class VMname(Resource):
                 
         abort(404, message="{} does not exist".format(name))
     
+
+    def post(self, name):
+        """
+        create vm
+        {
+            "host": "hogehoge.com (automatically decided if not specified)",
+            "cpu": {
+                "arch": "x86_64, ...",
+                "nvcpu": "number of vcpus",
+            },
+            "memory": "memory size of VM",
+            "disk": {
+                "pool": "pool name where disk is stored",
+                "size": "volume size"
+            },
+            "cdrom": "iso image path",
+            "mac_addr": "mac address (automatically generated if not specified)",
+            "vnc_password": "vnc password (no password if not specified)"
+        }
+        """
+
+        ##################################
+        #ここでvmが存在するかチェックする#
+        ##################################
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('host', type=str, location='json', required=False, default=None)
+        parser.add_argument('cpu', type=dict, location='json', required=True)
+        parser.add_argument('memory', type=dict, location='json', required=True)
+        parser.add_argument('disk', type=dict, location='json', required=True)
+        parser.add_argument('cdrom', type=str, location='json', required=True)
+        parser.add_argument('mac_addr', type=str, location='json', required=False, default=None)
+        parser.add_argument('vnc_password', type=str, location='json', required=False, default="")
         
+
