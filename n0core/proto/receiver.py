7f30d5551c210f5ac1479388bf5707a477bc3457
kyontan <kyontan@monora.me>
2017-10-03 01:59:30 +0900

    Define VM related schemas and restructured whole messages, remove compiled file

--- n0core/proto/receiver.py
+++ n0core/proto/receiver.py
@@ -1,8 +1,10 @@
-import all_pb2
+import n0stack_message_pb2 as message
 import pulsar
+import uuid
+import time
 import signal
 import sys
-
+import base64
 
 client = pulsar.Client('pulsar://localhost:6650')
 consumer = client.subscribe(
@@ -14,16 +16,49 @@ def signal_handler(signal, frame):
     client.close()
     sys.exit(0)
 
+
+def parse_message(received):
+    msg = message.N0stackMessage()
+    msg.ParseFromString(base64.b64decode(received))
+
+    msg_type = msg.WhichOneof('message')
+    sub_msg = getattr(msg, msg_type)
+    sub_msg_type = sub_msg.WhichOneof('message')
+
+    return getattr(sub_msg, sub_msg_type)
+
+
+def do_action(obj):
+    print("type: %s" % obj.__class__.__name__)
+    print(obj)
+
+    # Do something by type
+    if 'CreateVMRequest' == obj.__class__.__name__:
+        pass
+    elif 'UpdateVMRequest' == obj.__class__.__name__:
+        pass
+    elif 'DeleteVMRequest' == obj.__class__.__name__:
+        pass
+    elif 'UpdateVMPowerStateRequest' == obj.__class__.__name__:
+        pass
+    else:
+        pass
+
 signal.signal(signal.SIGINT, signal_handler)
 
 while True:
     try:
-      msg = consumer.receive(timeout_millis=10)
-      print("Received message: '%s'" % msg.data())
-      consumer.acknowledge(msg)
+        msg = consumer.receive(timeout_millis=10)
+        # print("received")
+        print("Received message: '%s'" % msg.data())
+        # print(msg.data())
+        do_action(parse_message(msg.data()))
+        consumer.acknowledge(msg)
     except Exception as e:
-      pass
+        if e.args[0] != 'Pulsar error: TimeOut':
+            print(e)
 
+        pass
 
 signal.pause()
 client.close()
--- n0core/proto/sender.py
+++ n0core/proto/sender.py
@@ -1,11 +1,29 @@
-import all_pb2
+import n0stack_message_pb2 as message
 import pulsar
+import uuid
+import time
+import base64
+
+
+def construct_message(obj, request_id='', type):
+    msg = message.N0stackMessage()
+    msg.version = 1
+    msg.epoch_us = int(time.time() * 1e6)
+
+    obj_type = obj.__class__.__name__
+
+    getattr(getattr(msg, type), obj_type).MergeFrom(obj)
+
+    return base64.b64encode(msg.SerializeToString())
+
+
+def send_message(producer, obj, request_id='', type='Request'):
+    msg = construct_message(obj, request_id=request_id, type=type)
+    print(msg)
+    producer.send(msg)
+
+    return None
 
 client = pulsar.Client('pulsar://localhost:6650')
 producer = client.create_producer(
     'persistent://sample/standalone/ns1/my-topic')
-
-for i in range(10):
-    producer.send('hello-pulsar-%d' % i)
-
-client.close()
