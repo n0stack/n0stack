ff3035ac3e0c0584c88b74b2ba335c32c789e006
palloc <palloc.mknet10@gmail.com>
2017-10-03 22:09:29 +0900

    add operation

---
+++ n0core/compute/operation/pool.py
@@ -0,0 +1,49 @@
+# coding: UTF-8
+from kvmconnect.base import BaseOpen
+from operation.xmllib.pool import PoolGen
+
+import os
+
+
+class Create(BaseOpen):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, pool_name, pool_path):
+        path = os.path.expandvars(pool_path)
+        if not os.path.exists(path):
+            try:
+                os.mkdir(path)
+            except PermissionError:
+                return False
+
+        poolgen = PoolGen()
+        poolgen(pool_name, path)
+
+        status = self.connection.storagePoolDefineXML(poolgen.xml, 0)
+        pool = self.connection.storagePoolLookupByName(pool_name)
+        pool.setAutostart(True)
+        pool.create()
+
+        if not status:
+            return False
+        else:
+            return True
+        
+
+class Delete(BaseOpen):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, name):
+        try:
+            pool = self.connection.storagePoolLookupByName(name)
+            try:
+                pool.destroy()
+            except:
+                pass
+            pool.undefine()
+        except:
+            return False
+
+        return True
