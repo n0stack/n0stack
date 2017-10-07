93bf69bff7ffb2734162a40e958f760953f8c4db
palloc <palloc.mknet10@gmail.com>
2017-10-07 17:22:59 +0900

    change some

--- n0core/compute/operation/volume.py
+++ n0core/compute/operation/volume.py
@@ -1,13 +1,14 @@
-# coding: UTF-8
 from kvmconnect.base import BaseOpen
 from operation.xmllib.volume import VolumeGen
 
 
 class Create(BaseOpen):
     def __init__(self):
+        # type: (...) -> None
         super().__init__()
 
     def __call__(self, pool_name, volume_name, size):
+        # type: (str, str, str) -> bool
         volgen = VolumeGen()
         volgen(volume_name, size)
 
@@ -25,9 +26,11 @@ class Create(BaseOpen):
 
 class Delete(BaseOpen):
     def __init__(self):
+        #type: (...) -> None
         super().__init__()
 
     def __call__(self, pool_name, volume_name):
+        # type: (str, str) -> bool
         try:
             pool = self.connection.storagePoolLookupByName(pool_name)
             storage = pool.storageVolLookupByName(volume_name)
