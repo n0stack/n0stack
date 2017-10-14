03246c872884cb3396751ac834115e29a497a02a
palloc <palloc.mknet10@gmail.com>
2017-10-14 21:25:45 +0900

    fix vm operation

--- n0core/compute/kvm/vm.py
+++ n0core/compute/kvm/vm.py
@@ -4,15 +4,13 @@ import enum
 import xml.etree.ElementTree as ET
 from typing import Any  # NOQA
 
-from xmllib import vm_generate
+from xmllib import xml_generate
+from base import QemuOpen
 
-
-class Status(QemuOpen):  # NOQA
+class VM(QemuOpen):  # NOQA
     """
     manage vm status
     """
-    status = enum.Enum('status', 'poweroff running')
-
     def __init__(self):
         # type: () -> None
         super().__init__()
@@ -68,80 +66,43 @@ class Status(QemuOpen):  # NOQA
 
         return True
 
-
-class Create(QemuOpen):  # NOQA
-    """
-    Create VM
-
-    parameters:
-        name: VM (domain) name
-        cpu:
-            arch: cpu architecture
-            nvcpu: number of vcpus
-        memory: memory size of VM
-        disk:
-            pool: pool name where disk is stored
-            size: volume size
-        cdrom: iso image path
-        mac_addr: mac address
-        vnc_password: vnc password
-    """
-    def __init__(self):
-        # type: () -> None
-        super().__init__()
-
-    def __call__(self,
-                 name,  # type: str
-                 cpu,  # type: str
-                 memory,  # type: str
-                 disk,  # type: Any
-                 cdrom,  # type: str
-                 mac_addr,  # type: str
-                 vnc_password  # type: str
-                 ):
+    def create(self,
+               name,  # type: str
+               cpu,  # type: str
+               memory,  # type: str
+               disk_path,  # type: str
+               cdrom,  # type: str
+               device,
+               mac_addr,  # type: str
+               vnc_password,  # type: str
+               nic_type
+               ):
         # type: (...) -> bool
-        vmgen = VmGen()
-
-        # create volume (disk)
-        volcreate = VolCreate()
-        if not volcreate(disk['pool'], name, disk['size']):
-            return False
-
+        
         # default values of nic
-        nic = {'type': 'bridge', 'source': 'virbr0', 'mac_addr': mac_addr, 'model': 'virtio'}
+        nic = {'type': 'bridge', 'source': device, 'mac_addr': mac_addr, 'model': nic_type}
 
-        pool = self.conn.storagePoolLookupByName(disk['pool'])
-        vol = pool.storageVolLookupByName(name+'.img')
+        vm_xml = xml_generate(name,
+                              cpu,
+                              memory,
+                              disk_path,
+                              cdrom,
+                              nic,
+                              vnc_password)
 
-        vmgen(name, cpu, memory, vol.path(), cdrom, nic, vnc_password)
-
-        dom = self.conn.createXML(vmgen.xml, 0)
+        dom = self.conn.createXML(vm_xml, 0)
 
         if not dom:
             return False
         else:
             return True
 
-
-class Delete(QemuOpen):  # NOQA
-    """
-    Delete VM
-    """
-    def __init__(self):
-        # type: () -> None
-        super().__init__()
-
-    def __call__(self, name):
+    def delete(self, name):
         # type: (str) -> bool
         try:
             vdom = self.conn.lookupByName(name)
             if vdom.isActive():
                 vdom.shutdown()
-
-            # delete matched volume
-            vol = self.conn.volumeLookupByName(name)
-            vol.wipe(0)
-            vol.delete(0)
 
             if vdom.isActive():
                 vdom.destroy()
@@ -153,75 +114,3 @@ class Delete(QemuOpen):  # NOQA
             return False
 
         return True
-
-
-class Clone(QemuOpen):  # NOQA
-    """
-    Clone VM
-
-    parameters:
-        src: original vm name
-        dst: new vm name
-    """
-    def __init__(self):
-        # type: () -> None
-        super().__init__()
-
-    def __call__(self,
-                 src,  # type: str
-                 dst,  # type: str
-                 vncpass  # type: str
-                 ):
-        # type: (...) -> bool
-        srcdom = self.conn.lookupByName(src)
-        # if srcdom.isActive(): # if vm is up
-        #     # TODO: save state or something
-        #     return False
-
-        # clone volume from src to dst
-        srcvol = self.conn.volumeLookupByName(src)
-        volgen = VolumeGen()
-        dst_cap = srcvol.info()[1]
-        volgen(dst, str(dst_cap)+'B')
-        pool = srcvol.storagePoolLookupByVolume()
-        status = pool.createXMLFrom(volgen.xml, srcvol)
-
-        if not status:
-            return False
-
-        dstvol = self.conn.volumeLookupByName(dst)
-
-        # clone VM
-        # copy XML from src
-        root = ET.fromstring(srcdom.XMLDesc())  # type: Any
-        # replace name
-        el_name = root.find('./name')  # type: Any
-        el_name.text = dst
-        # remove uuid
-        el_uuid = root.find('./uuid')  # type: Any
-        root.remove(el_uuid)
-        # replace disk
-        el_disk = root.find("./devices/disk[@device='disk']/source")  # type: Any
-        el_disk.set('file', dstvol.path())
-        # remove mac addr
-        el_interface = root.find("./devices/interface[@type='bridge']")  # type: Any
-        el_mac = el_interface.find('./mac')  # type: Any
-        el_interface.remove(el_mac)
-        # remove serial and console
-        el_devices = root.find('./devices')  # type: Any
-        el_devices.remove(el_devices.find("./serial[@type='pty']"))
-        el_devices.remove(el_devices.find("./console[@type='pty']"))
-        # reset vnc port and vnc password
-        el_graphics = root.find("./devices/graphics")  # type: Any
-        el_graphics.set('port', '-1')
-        el_graphics.set('passwd', vncpass)
-        # remove seclabel
-        root.remove(root.find('./seclabel'))
-
-        dst_xml = ET.tostring(root).decode()
-        dstdom = self.conn.createXML(dst_xml, 0)
-
-        if not dstdom:
-            return False
-        else:
-            return True
