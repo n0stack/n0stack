dcb3dcac3fea8733d7eb2a216fe9bd4af8aff94d
palloc <palloc.mknet10@gmail.com>
2017-10-15 16:43:13 +0900

    change files

--- n0core/compute/compute.py
+++ n0core/compute/compute.py
@@ -3,19 +3,17 @@ sys.path.append('../../')
 import pulsar
 
 from n0core.lib.proto import UpdateVMPowerStateRequest
-
+from n0core.lib.messenger import Messenger
 
 def main():
     client = pulsar.Client('pulsar://localhost:6650')
     consumer = client.subscribe('persistent://sample/standalone/compute/114514',
                                 subscription_name='compute')
     while True:
-        msg = consumer.receive()
-        data = msg.data().encode('utf-8')
-        deserialized = UpdateVMPowerStateRequest()
-        deserialized.ParseFromString(data)
-        print(deserialized)
-        consumer.acknowledge(msg)
+
+        inner_msg, messenger = Messenger.receive_message(consumer)
+        print(inner_msg)
+        print(messenger.request_id)
         
     client.close()
 
