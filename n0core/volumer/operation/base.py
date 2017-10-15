b278d0c86ddb89b50bead731ac3795b7310902b9
icchy <icchyr@gmail.com>
2017-10-15 12:14:09 +0900

    implemented volume create/delete, pool init

--- n0core/volumer/operation/base.py
+++ n0core/volumer/operation/base.py
@@ -1,26 +1,61 @@
+from .xmllib import build_pool
+
+from n0library.logger import Logger
+
 import libvirt
 import sys
+import os
+
+
+POOL_NAME = 'n0stack'
+POOL_PATH = '/var/lib'
+QEMU_URL = 'qemu:///system'
+logger = Logger()
 
 
 class BaseReadOnly:
-    QEMU_URL = "qemu:///system"
-
     def __init__(self):
+        # type: () -> None
         try:
-            conn = libvirt.openReadOnly(self.QEMU_URL)
+            conn = libvirt.openReadOnly(QEMU_URL)
         except libvirt.libvirtError as e:
-            print(e)
+            logger.error('unabled to connect to libvirt')
             sys.exit(1)
+
+        pool = conn.storagePoolLookupByName(POOL_NAME)
+        if pool is None:
+            _init_pool(self)
         self.conn = conn
+        self.pool = pool
 
 
 class BaseOpen:
-    QEMU_URL = "qemu:///system"
-
     def __init__(self):
+        # type: () -> None
         try:
-            conn = libvirt.open(self.QEMU_URL)
+            conn = libvirt.open(QEMU_URL)
         except libvirt.libvirtError as e:
-            print(e)
+            logger.error('unabled to connect to libvirt')
             sys.exit(1)
+
+        pool = conn.storagePoolLookupByName(POOL_NAME)
+        if pool is None:
+            _init_pool(self)
         self.conn = conn
+        self.pool = pool
+
+
+def _init_pool(cls):
+    path = POOL_PATH
+    if not os.path.exists(path):
+        # TODO: error log
+        logger.critical('not such pool path {}'.format(path))
+        sys.exit(1)
+
+    xml = build_pool(POOL_NAME, path)
+    pool = cls.conn.storagePoolDefineXML(xml, 0)
+    if pool is None:
+        logger.critical('failed to define pool')
+        sys.exit(1)
+    pool.setAutostart(True)
+    pool.create()
