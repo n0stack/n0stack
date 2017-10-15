69c5fc1abfdc6e14250bd45639603a5612e623aa
Naoki Kanazawa <kanazawa@naoki912.net>
2017-10-15 19:54:41 +0900

    Fixed Arguments of Scheduler

--- n0core/scheduler/main.py
+++ n0core/scheduler/main.py
@@ -1,4 +1,5 @@
 import sys
+from urllib.parse import urlparse
 
 from n0library.arguments.common import CommonArguments
 from scheduler import Scheduler
@@ -10,49 +11,31 @@ sys.path.append('../../')
 def main():
     argparser = CommonArguments(
         description="",
-    ) # type: CommonArguments
+    )
 
-    argparser.add_argument("--db-user",
+    argparser.add_argument("--db-url",
                            type=str,
                            default=None,
-                           dest='db_user',
+                           dest="db_url",
                            help="")
-    argparser.add_argument("--db-host",
+    argparser.add_argument("--prometheus-url",
                            type=str,
                            default=None,
-                           dest='db_host',
-                           help="")
-    argparser.add_argument("--db-port",
-                           type=int,
-                           default=None,
-                           dest='db_port',
-                           help="")
-    argparser.add_argument("--db-password",
-                           type=str,
-                           default=None,
-                           dest='db_password',
-                           help="")
-    argparser.add_argument('--prometheus-host',
-                           type=str,
-                           default=None,
-                           dest='prometheus_host',
-                           help="")
-    argparser.add_argument('--prometheus-port',
-                           type=int,
-                           default=None,
-                           dest='prometheus_port',
+                           dest="prometheus_url",
                            help="")
     args = argparser.parse_args()
 
+    db_url = urlparse(args.db_url)
+    prometheus_url = urlparse(args.prometheus_url)
 
     scheduler = Scheduler(
-        db_user=args.db_user,
-        db_host=args.db_host,
-        db_port=str(args.db_port),
-        db_password=args.db_password,
-        prometheus_host=args.prometheus_host,
-        prometheus_port=str(args.prometheus_port),
-    ) # type: Scheduler
+        db_user=args.db_url.username,
+        db_host=args.db_url.hostname,
+        db_port=str(db_url.port),
+        db_password=db_url.password,
+        prometheus_host=prometheus_url.hostname,
+        prometheus_port=str(prometheus_url.port),
+    )
 
 
 if __name__ == '__main__':
