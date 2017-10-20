ba96e74df3cbe6c9ffe8e44b19c782a8c2f36097
icchy <icchyr@gmail.com>
2017-10-20 22:24:04 +0900

    fix flake8 error

--- n0core/volumer/test_listener.py
+++ n0core/volumer/test_listener.py
@@ -9,11 +9,13 @@ except:
 client = N0MQ('pulsar://localhost:6650/')
 consumer = client.subscribe('persistent://sample/standalone/volumer/114514')
 
+
 @consumer.on('CreateVolumeRequest')
 def create_volume_request(message):
     print('CreateVolumeRequest')
     print(message.data)
     # consumer.ack(message)
+
 
 @consumer.on('DeleteVolumeRequest', auto_ack=False)
 def delete_volume_request(message):
