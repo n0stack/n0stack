d87cd7961ebe50aeecf37d0cb2838542dff1928a
palloc <palloc.mknet10@gmail.com>
2017-10-22 00:15:57 +0900

    change main

--- n0core/compute/main.py
+++ n0core/compute/main.py
@@ -1,21 +1,21 @@
-from mqhandler import MQHandler
+import sys
+sys.path.append('../../')  # NOQA
+import pulsar
 
-mqhandler = MQHandler('pulsar://localhost:6550',
-                      'persistent://sample/standalone/volumer/114514',
-                      subscription_name='compute')
-
-
-@mqhandler.handle('CreateVMRequest')
-def create_VM_handler(inner_msg, messenger):
-    print('create vm')
-    print(inner_msg)
+from n0core.lib.n0mq import N0MQ
+from n0core.lib.proto import CreateVMRequest
 
 
-@mqhandler.handle('DeleteVMRequest')
-def delete_vm_handler(inner_msg, messenger):
-    print('delete vm')
-    print(inner_msg)
+client = N0MQ('pulsar://localhost:6650')
+consumer = client.subscribe('persistent://sample/standalone/compute/handle')
 
 
+@consumer.on('CreateVMRequest')
+def create_VM_request(message, auto_ack=False):
+    print('create vm request')
+    print(message)
+    consumer.ack(message)
+
+    
 if __name__ == '__main__':
-    mqhandler.listen()
+    client.listen()
