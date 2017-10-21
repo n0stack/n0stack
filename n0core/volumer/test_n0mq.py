9d67fb4302998c483c2a148d123cad4246dd4abb
icchy <icchyr@gmail.com>
2017-10-22 01:17:42 +0900

    update volumer and test

--- n0core/volumer/test_n0mq.py
+++ n0core/volumer/test_n0mq.py
@@ -5,18 +5,28 @@ except:
     sys.path.append('../../')
     from n0core.lib.n0mq import N0MQ
 
+from uuid import uuid4 as uuid
+
 from n0core.lib.proto import CreateVolumeRequest, DeleteVolumeRequest
 
 
+client = N0MQ('pulsar://localhost:6650')
+producer = client.create_producer('persistent://sample/standalone/volumer/test')
+
+
+def test_create(id, host, size_mb):
+    req = CreateVolumeRequest(id=id, host=host, size_mb=size_mb)
+    producer.send(req)
+
+
+def test_delete(id, host):
+    req = DeleteVolumeRequest(id=id, host=host)
+    producer.send(req)
+
+
 def main():
-    client = N0MQ('pulsar://localhost:6650')
-    producer = client.create_producer('persistent://sample/standalone/volumer/test')
-
-    req = CreateVolumeRequest(id='1', host='test', size_mb=1)
-    producer.send(req)
-
-    req = DeleteVolumeRequest(id='1', host='test')
-    producer.send(req)
+    # test_create(str(uuid()), 'test', 1024)
+    test_delete(str(uuid()), 'test')
 
 
 if __name__ == '__main__':
--- n0core/volumer/volumer.py
+++ n0core/volumer/volumer.py
@@ -5,23 +5,39 @@ except:
     sys.path.append('../../')
     from n0core.lib.n0mq import N0MQ
 
+from n0library.logger import Logger
+from operation import Volume
+
 
 client = N0MQ('pulsar://localhost:6650')
 consumer = client.subscribe('persistent://sample/standalone/volumer/test')
+volume = Volume()
+logger = Logger()
 
 
 @consumer.on('CreateVolumeRequest')
 def on_create_volume_req(message):
-    print('CreateVolumeRequest')
-    req = message.data
-    print(req.id)
+    logger.info('CreateVolumeRequest')
+
+    data = message.data
+    host = data.host
+    size_mb = data.size_mb
+
+    if not volume.create(host, size_mb):
+        logger.error('unable to create volume: {}'.format(str(data)))
+        return False
 
 
 @consumer.on('DeleteVolumeRequest')
 def on_delete_volume_req(message):
-    print('DeleteVolumeRequest')
-    req = message.data
-    print(req.id)
+    logger.info('DeleteVolumeRequest')
+
+    data = message.data
+    host = data.host
+
+    if not volume.delete(host):
+        logger.error('unable to delete volume: {}'.format(str(data)))
+        return False
 
 
 if __name__ == '__main__':
