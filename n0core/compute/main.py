36f201cbdc3cfee078a46e9a2ab9dd6f4236f612
palloc <palloc.mknet10@gmail.com>
2017-10-22 00:38:27 +0900

    fix main

--- n0core/compute/main.py
+++ n0core/compute/main.py
@@ -3,7 +3,7 @@ sys.path.append('../../')  # NOQA
 import pulsar
 
 from n0core.lib.n0mq import N0MQ
-from n0core.lib.proto import CreateVMRequest
+from n0core.lib.proto import CreateVMRequest  # NOQA
 
 
 client = N0MQ('pulsar://localhost:6650')
@@ -19,25 +19,3 @@ def create_VM_request(message, auto_ack=False):
     
 if __name__ == '__main__':
     client.listen()
-=======
-from mqhandler import MQHandler
-
-mqhandler = MQHandler('pulsar://localhost:6550',
-                      'persistent://sample/standalone/volumer/114514',
-                      subscription_name='compute')
-
-
-@mqhandler.handle('CreateVMRequest')
-def create_VM_handler(inner_msg, messenger):
-    print('create vm')
-    print(inner_msg)
-
-
-@mqhandler.handle('DeleteVMRequest')
-def delete_vm_handler(inner_msg, messenger):
-    print('delete vm')
-    print(inner_msg)
-
-
-if __name__ == '__main__':
-    mqhandler.listen()
