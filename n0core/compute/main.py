0a4631abc813b6269b3045f6eb25559975522362
palloc <palloc.mknet10@gmail.com>
2017-10-22 19:14:24 +0900

    add handling process

--- n0core/compute/main.py
+++ n0core/compute/main.py
@@ -1,9 +1,13 @@
 import sys
 sys.path.append('../../')  # NOQA
-import pulsar
 
 from n0core.lib.n0mq import N0MQ
-from n0core.lib.proto import CreateVMRequest  # NOQA
+from n0core.lib.proto import (CreateVMRequest,
+                              DeleteVMRequest,
+                              UpdateVMRequest,
+                              UpdateVMPowerStateRequest)  # NOQA
+
+from n0core.compute.kvm import vm
 
 
 client = N0MQ('pulsar://localhost:6650')
@@ -16,6 +20,27 @@ def create_VM_request(message, auto_ack=False):
     print(message.data)
     consumer.ack(message)
 
-    
+
+@consumer.on('DeleteVMRequest')
+def delete_VM_request(message, auto_ack=False):
+    print('delete vm request')
+    print(message.data)
+    consumer.ack(message)
+
+
+@consumer.on('UpdateVMRequest')
+def update_VM_request(message, auto_ack=False):
+    print('update vm request')
+    print(message.data)
+    consumer.ack(message)
+
+
+@consumer.on('UpdateVMPowerStateRequest')
+def update_VM_power_state_request(message, auto_ack=False):
+    print('update vm power state request')
+    print(message.data)
+    consumer.ack(message)
+
+
 if __name__ == '__main__':
     client.listen()
