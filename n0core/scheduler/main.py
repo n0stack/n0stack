23e65299be5b0135459e009159f5c616a3c428ac
yatuhashi kei <keyansuiya@gmail.com>
2017-10-25 03:41:24 +0900

    fix def main & add agent file

--- n0core/scheduler/main.py
+++ n0core/scheduler/main.py
@@ -1,38 +1,49 @@
 from urllib.parse import urlparse
 
 from n0library.arguments.common import CommonArguments
-from scheduler import Scheduler
+from n0library.logger import Logger
+from n0core.lib.n0mq import N0MQ
+
+from .scheduler import Scheduler
+import compute  # NOQA
+import porter  # NOQA
+import volumer  # NOQA
+import networker  # NOQA
 
 
-def main():
-    parser = CommonArguments(
-        description="",
-    )
+client = N0MQ('pulsar://localhost:6650')
+consumer = client.subscribe('persistent://scheduler')
+logger = Logger()
 
-    parser.add_argument("--database-url",
-                        type=str,
-                        default=None,
-                        dest="db_url",
-                        help="Database URL")
-    parser.add_argument("--prometheus-url",
-                        type=str,
-                        default=None,
-                        dest="prometheus_url",
-                        help="Prometheus URL")
-    args = parser.parse_args()
 
-    db_url = urlparse(args.db_url)
-    prometheus_url = urlparse(args.prometheus_url)
+parser = CommonArguments(
+    description="",
+)
 
-    scheduler = Scheduler(
-        db_user=db_url.username,
-        db_host=db_url.hostname,
-        db_port=str(db_url.port),
-        db_password=db_url.password,
-        prometheus_host=prometheus_url.hostname,
-        prometheus_port=str(prometheus_url.port),
-    )
+parser.add_argument("--database-url",
+                    type=str,
+                    default=None,
+                    dest="db_url",
+                    help="Database URL")
+parser.add_argument("--prometheus-url",
+                    type=str,
+                    default=None,
+                    dest="prometheus_url",
+                    help="Prometheus URL")
+args = parser.parse_args()
+
+db_url = urlparse(args.db_url)
+prometheus_url = urlparse(args.prometheus_url)
+
+scheduler = Scheduler(
+    db_user=db_url.username,
+    db_host=db_url.hostname,
+    db_port=str(db_url.port),
+    db_password=db_url.password,
+    prometheus_host=prometheus_url.hostname,
+    prometheus_port=str(prometheus_url.port),
+)
 
 
 if __name__ == '__main__':
-    main()
+    client.listen()
--- n0core/scheduler/scheduler.py
+++ n0core/scheduler/scheduler.py
@@ -3,9 +3,16 @@ import sqlalchemy as sa
 from ResorceCalculation import Execfunc
 
 
+def CheckHost(msg):
+    if msg.host is None:
+        print("host")
+    return send(msg.host, msg.process)
+
+
 class Scheduler():
     """
-    Scheduler main class
+    Scheduler Method
+    各protoタイプで実行するようなjobをメソッド化しておく
     """
     # postgresql+psycopg2://user:password@host:port/dbname[?key=value&key=value...]
     DB_URL = None
@@ -22,18 +29,13 @@ class Scheduler():
         self.PROMETHEUS_HOST = prometheus_host
         self.PROMETHEUS_PORT = prometheus_port
 
-    def MQReceived(self, msg):
-        if msg.host is None:
-            self.scheduleJob()
-        return self.send(msg.host, msg.process)
-
     def ScheduleJob(self):
         re = Resorce()
         re.DBResorce(self.DB_URL)
         re.PrometheusRresorce(self.PROMETHEUS_HOST, self.PROMETHEUS_PORT)
         return re.ResorceCalculation(Execfunc)
 
-    def Send(proto, host, process):
+    def Send(proto, topic):
         return
 
 
@@ -41,26 +43,29 @@ class Resorce():
     """
     各リソースをclass変数でもっておき、メソッドが実行で取得し初期化する
     """
-    DB_CPU = None
-    DB_MEMORY = None
-    DB_DISKFREE = None
-    Prometheus_CPU = None
-    Prometheus_MEMORY = None
-    Prometheus_DISKFREE = None
+    DB_CPU = {}
+    DB_MEMORY = {}
+    DB_DISKFREE = {}
+    Prometheus_CPU = {}
+    Prometheus_MEMORY = {}
+    Prometheus_DISKFREE = {}
     CREATE_VM_CPU = None
     CREATE_VM_MEMORY = None
     CREATE_VM_DISK = None
 
-    def DBResorce(db_url):
+    def DBResorce(self, db_url):
         engine = sa.create_engine(db_url, echo=True)
         Session = sa.orm.sessionmaker(bind=engine)
         session = Session()
         hosts = session.query('HOST').all()
-        return hosts
+        for h in hosts:
+            self.DB_CPU.append(h.cpu)
+            self.DB_MEMORY.append(h.memory)
+            self.DB_DISKFREE.append(h.diskfree)
 
-    def PrometheusRresorce():
-        pass
+    def PrometheusRresorce(self):
+        return
 
-    def ResorceCalculation(Execfunc):
+    def ResorceCalculation(self, Execfunc):
         # Resorce 計算アルゴリズムは、後で自由に変えてくれという気持ち
         return Execfunc()
