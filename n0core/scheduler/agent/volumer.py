da20e7f6e0fed21e809ed7418a6ca962c91d21fb
yatuhashi kei <keyansuiya@gmail.com>
2017-10-26 11:27:27 +0900

    add agent volumer

--- n0core/scheduler/agent/volumer.py
+++ n0core/scheduler/agent/volumer.py
@@ -1,3 +1,4 @@
+import pulsar  # NOQA
 from initialize import consumer, logger, send, volumer_producer  # NOQA
 try:
     from n0core.lib import proto
@@ -5,3 +6,26 @@ except:  # NOQA
     import sys
     sys.path.append('../../')
     from n0core.lib import proto  # NOQA
+
+
+@consumer.on('CreateVolumeRequest')
+def create_volume_req(message):
+    # type: (pulsar.Message, bool) -> bool
+    logger.info('CreateVolumeRequest')
+    data = message.data
+    req = proto.CreateVolumeRequest(id=data.id,
+                                    host=data.host,
+                                    size_mb=data.size_mb)
+    send(volumer_producer + data.host, req)
+    consumer.ack(message)
+
+
+@consumer.on('DeleteVolumeRequest')
+def delete_volume_req(message):
+    # type: (pulsar.Message, bool) -> bool
+    logger.info('DeleteVolumeRequest')
+    data = message.data
+    req = proto.DetachVolumeRequest(id=data.id,
+                                    host=data.host)
+    send(volumer_producer + data.host, req)
+    consumer.ack(message)
