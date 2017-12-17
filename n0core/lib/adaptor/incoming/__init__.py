d947693335d502c4d31801d35b3e73f1b9b1e80d
h-otter <h-otter@outlook.jp>
2017-12-18 05:47:06 +0900

    make incoming and outgoing adapter to one object

--- n0core/lib/adaptor/incoming/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Any
-
-from n0core.lib.adaptor import Adapter
-
-class IncomingAdapter(Adapter):
-    def receive(self):
-        # type: () -> Dict[str, Any]
-        raise NotImplementedError
--- n0core/lib/adaptor/outgoing/__init__.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Any
-
-from n0core.lib.adaptor import Adapter
-
-class OutgoingAdapter(Adapter):
-    def send(self, message):
-        # type: (Dict[str, Any]) -> None
-        raise NotImplementedError
