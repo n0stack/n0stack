c8d12dd150f04f2bd44a4b15953c5afd6b298ab3
h-otter <h-otter@outlook.jp>
2017-12-18 05:47:06 +0900

    make incoming and outgoing adapter to one object

--- n0core/lib/adaptor/__init__.py
+++ n0core/lib/adaptor/__init__.py
@@ -1,4 +1,21 @@
+from typing import Dict, Any
+
 class Adapter:
-    def __init__(self):
-        # type: () -> None
-        pass
+    """
+    Adapters provide presentation layer.
+    """
+
+    def receive(self):
+        # type: () -> Dict[str, Any]
+        raise NotImplementedError
+
+    def send(self, message):
+        # type: (Dict[str, Any], List[Dict[str, Any]]) -> None
+        """
+        This method send message to default destination.
+
+        Args:
+            header:
+            objects:
+        """
+        raise NotImplementedError
