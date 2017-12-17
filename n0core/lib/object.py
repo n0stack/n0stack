b0f8290fefdc19c98bcbd6669e659e5cf125ce16
h-otter <h-otter@outlook.jp>
2017-12-18 06:40:07 +0900

    add example

--- n0core/lib/object.py
+++ n0core/lib/object.py
@@ -1,21 +1,30 @@
+from uuid import uuid4
 from typing import Dict, List  # NOQA
 
 
 class Object(dict):
     """
+    Example:
+        >>> new_disk = Object("resource/volume/local", "claimed")
+        >>> new_disk["size"] = 100 * 1024 * 1024 * 1024
+        >>> new_disk.meta["n0stack/n0core/resource/vm/boot_priority"] = "1"
 
     TODO:
         - dependencyの2重定義ができないようにしたい
     """
     def __init__(self,
-                 id,              # str
                  type,            # str
                  state,           # str
+                 id="",           # str
                  meta={},         # Dict[str, str]
                  dependencies=[]  # List[Dependency]
                  ):
         # type: (...) -> None
-        self.__id = id
+        if id:
+            self.__id = id
+        else:
+            self.__id = uuid4()
+
         self.__type = type
         self.state = state
         self.meta = meta
@@ -32,6 +41,11 @@ class Object(dict):
 
 class Dependency():
     """
+    Example:
+        >>> new_vm = Object("resource/vm/kvm", "running")
+        >>> new_disk = Object("resource/volume/local", "claimed")
+        >>> new_dependency = Dependency(new_disk, "n0stack/n0core/resource/vm/attachments")
+        >>> new_vm.dependencies.append(new_dependency)
 
     TODO:
         - labelを書き込み可能にするか否か
