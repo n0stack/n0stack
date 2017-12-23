f882e6fd44b038f86b5ca0df5977d3ce0fbde470
h-otter <h-otter@outlook.jp>
2017-12-24 02:02:46 +0900

    try to develop

---
+++ n0core/lib/target.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+from n0core.lib.model import Model  # NOQA
+
+
+class Target(object):
+    def __init__(self):
+        # type: () -> None
+        pass
+
+    def apply(self, model):
+        # type: (Model) -> Tuple[bool, str]
+        pass
