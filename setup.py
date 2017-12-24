090d0ce346adde628c32ebfd081234e0b344e871
h-otter <h-otter@outlook.jp>
2017-12-25 02:00:15 +0900

    test on docker

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    install_requires = [
-        'protobuf',
-        'pulsar-client',
-        'pyroute2',
-        'python-iptables',
-        'git+https://github.com/larsks/python-netns.git',
-    ]
-
     with open('README.md') as f:
         readme = f.read()
 
