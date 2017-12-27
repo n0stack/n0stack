c133d09d1a7181bd9a2ebe214491520885f786de
kyontan <kyontan@monora.me>
2017-12-28 01:54:42 +0900

    Rename notify to notification

--- n0core/processor/agent.py
+++ n0core/processor/agent.py
@@ -3,7 +3,7 @@ from typing import List  # NOQA
 from n0core.processor import Processor
 from n0core.processor import IncompatibleMessage
 from n0core.message import Message
-from n0core.message.notify import Notify
+from n0core.message.notification import Notification
 from n0core.target import Target  # NOQA
 from n0core.gateway import Gateway  # NOQA
 
@@ -12,27 +12,27 @@ class Agent(Processor):
     def __init__(self,
                  target,       # type: Target
                  model_types,  # type: List[str]
-                 notify        # type: Gateway
+                 notification  # type: Gateway
                  ):
         # type: (...) -> None
         self.__target = target
         self.__model_types = model_types
-        self.__notify = notify
+        self.__notification = notification
 
     def proccess(self, message):
-        # type: (Notify) -> None
-        if message.type is not Message.TYPES.NOTIFY:
+        # type: (Notification) -> None
+        if message.type is not Message.TYPES.NOTIFICATION:
             raise IncompatibleMessage
         if message.model.type not in self.__model_types:
             raise IncompatibleMessage
         if not message.succeeded:
             raise IncompatibleMessage
 
-        m, s, d = self.__target.apply(message.model)
-        n = Notify(spec_id=message.spec_id,
-                   model=m,
-                   event=Notify.EVENTS.APPLIED,
-                   succeeded=s,
-                   description=d)
+        model, succeeded, description = self.__target.apply(message.model)
+        notification = Notification(spec_id=message.spec_id,
+                                    model=model,
+                                    event=Notification.EVENTS.APPLIED,
+                                    succeeded=succeeded,
+                                    description=description)
 
-        self.__notify.send(n)
+        self.__notification.send(notification)
