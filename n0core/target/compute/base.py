4a632e685af2b69173557d356410dd9fccecbb4c
palloc <palloc.mknet10@gmail.com>
2017-12-28 04:45:41 +0900

    add some functions

---
+++ n0core/target/compute/base.py
@@ -0,0 +1,68 @@
+from typing import Any  # NOQA
+import os
+import sys
+
+import libvirt
+
+from n0library.logger import Logger
+from .xmllib import build_pool
+
+
+POOL_NAME = 'n0stack'
+POOL_PATH = '/var/lib/{}'.format(POOL_NAME)
+QEMU_URL = 'qemu:///system'
+logger = Logger()
+
+
+class QemuReadOnly:
+    def __init__(self):
+        # type: () -> None
+        try:
+            conn = libvirt.openReadOnly(QEMU_URL)
+        except libvirt.libvirtError as e:
+            logger.error('unable to connect to libvirt')
+            sys.exit(1)
+
+        try:
+            pool = conn.storagePoolLookupByName(POOL_NAME)
+        except libvirt.libvirtError:
+            _init_pool(conn)
+            pool = conn.storagePoolLookupByName(POOL_NAME)
+
+        self.conn = conn
+        self.pool = pool
+
+
+class QemuOpen:
+    def __init__(self):
+        # type: () -> None
+        try:
+            conn = libvirt.open(QEMU_URL)
+        except libvirt.libvirtError as e:
+            logger.error('unable to connect to libvirt')
+            sys.exit(1)
+
+        try:
+            pool = conn.storagePoolLookupByName(POOL_NAME)
+        except libvirt.libvirtError:
+            _init_pool(conn)
+            pool = conn.storagePoolLookupByName(POOL_NAME)
+        self.conn = conn
+        self.pool = pool
+
+
+def _init_pool(conn):
+    # type: (Any) -> None
+    path = POOL_PATH
+
+    if not os.path.exists(path):
+        logger.critical('not such pool path {}'.format(path))
+        sys.exit(1)
+
+    xml = build_pool(POOL_NAME, path)
+    pool = conn.storagePoolDefineXML(xml, 0)
+    if pool is None:
+        logger.critical('failed to define pool')
+        sys.exit(1)
+    pool.setAutostart(True)
+    pool.create()
---
+++ n0core/target/compute/libvirt_kvm.py
@@ -0,0 +1,221 @@
+import time
+import libvirt
+from typing import Any  # NOQA
+
+from n0core.model import Model  #NOQA
+from n0core.target.compute.xmllib import (define_vm_xml,
+                                          define_volume_xml,
+                                          build_volume,
+                                          build_network)
+from n0core.target.compute.base import QemuOpen
+from n0core.target import Target
+
+
+class VM(QemuOpen, Target):  # NOQA
+    """
+    manage vm status
+    """
+    def __init__(self):
+        # type: () -> None
+        super().__init__()
+
+    def apply(self, model):
+        # type: (Model) -> Tuple[Model, bool, str]
+
+        if self.start(model.name):
+            # TODO: error process
+            return model, False, "faild"
+
+        return model, True, "succeeded"
+
+    def start(self, name):
+        # type: (str) -> bool
+        domain = self.conn.lookupByName(name)
+        domain.create()
+
+        # fail if over 120 seconds
+        s = time.time()
+        while True:
+            if domain.info()[0] == 1:
+                break
+            if time.time() - s > 120:
+                return False
+
+        return True
+
+    def stop(self, name):
+        # type: (str) -> bool
+        domain = self.conn.lookupByName(name)
+        domain.shutdown()
+
+        # fail if over 120 seconds
+        s = time.time()
+        while True:
+            if domain.info()[0] != 1:
+                break
+            if time.time() - s > 120:
+                return False
+
+        return True
+
+    def force_stop(self, name):
+        # type: (str) -> bool
+        domain = self.conn.lookupByName(name)
+        domain.destroy()
+
+        # fail if over 60 seconds
+        s = time.time()
+        while True:
+            if domain.info()[0] != 1:
+                break
+            if time.time() - s > 60:
+                # TODO: add error log
+                return False
+
+        return True
+
+    def create(self,
+               name,  # type: str
+               cpu,  # type: Any
+               memory,  # type: str
+               disk_path,  # type: str
+               cdrom,  # type: str
+               device,  # type: Any
+               mac_addr,  # type: str
+               vnc_password,  # type: str
+               nic_type  # type: Any
+               ):
+        # type: (...) -> bool
+
+        # default values of nic
+        nic = {'type': 'bridge', 'source': device, 'mac_addr': mac_addr, 'model': nic_type}
+
+        vm_xml = define_vm_xml(name,
+                               cpu,
+                               memory,
+                               disk_path,
+                               cdrom,
+                               nic,
+                               vnc_password)
+
+        dom = self.conn.createXML(vm_xml, 0)
+
+        if not dom:
+            # TODO: error log
+            return False
+
+        return True
+
+    def delete(self, name):
+        # type: (str) -> bool
+        try:
+            vdom = self.conn.lookupByName(name)
+            if vdom.isActive():
+                vdom.shutdown()
+
+            if vdom.isActive():
+                vdom.destroy()
+            else:
+                vdom.undefine()
+
+        except libvirt.libvirtError as e:
+            # TODO: error log
+            print(e)
+            return False
+
+        return True
+
+    def update(self, name, vcpus, memory):
+        # type: (str, int, int) -> bool
+        try:
+            dom = self.conn.lookupByName(name)
+            dom.setMemory(vcpus)
+            dom.setMemory(memory)
+
+        except libvirt.libvirtError as e:
+            # TODO: error log
+            print(e)
+            return False
+
+        return True
+
+    def attach_volume(self, name, volume_id):
+        # type: (str, str) -> bool
+        vm = self.conn.lookupByName(name)
+
+        xml = define_volume_xml("/var/lib/n0stack/" + volume_id)
+        vm.attachDevice(xml)
+
+        return True
+
+    def attach_nic(self, name, network_name, mac_address):
+        # type: (str, str) -> bool
+
+        # TODO: mac_address
+        vm = self.conn.lookupByName(name)
+
+        xml = define_interface_xml(network_name, mac_address)
+        vm.attachDevice(xml)
+
+        return True
+
+
+class Volume(QemuOpen):
+    def __init__(self):
+        # type: () -> None
+        super().__init__()
+
+    def create(self, name, size):
+        # type: (str, str) -> bool
+        xml = build_volume(name, size)
+
+        if self.pool.createXML(xml) is None:
+            # TODO: error log
+            return False
+
+        return True
+
+    def delete(self, name, wipe=True):
+        # type: (str, bool) -> bool
+        storage = self.pool.storageVolLookupByName(name+'.img')
+
+        if storage is None:
+            # TODO: error log
+            return False
+
+        if wipe:
+            storage.wipe(0)
+        storage.delete(0)
+
+        return True
+
+
+class Network(QemuOpen):
+    def __init__(self):
+        # type: () -> None
+        super().__init__()
+
+    def create(self,
+               network_name,  # type: str
+               bridge_name,  # type: str
+               address,  # type: str
+               netmask,  # type: str
+               range_start,  # type: str
+               range_end  # type: str
+               ):
+        # type: (...) -> bool
+        xml = build_network(network_name,
+                            bridge_name,
+                            address,
+                            netmask,
+                            range_start,
+                            range_end)
+
+        network = self.conn.networkCreateXML(xml)
+
+        if network is None:
+            # TODO: error log
+            print('Failed to create a virtual network')
+            return False
+
+        return True
