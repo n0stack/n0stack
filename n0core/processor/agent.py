ec1aea3bbb36fb3dcb1cc09dddfada131359527e
h-otter <h-otter@outlook.jp>
2017-12-29 22:58:47 +0900

    support to process message on targets

--- n0core/processor/agent.py
+++ n0core/processor/agent.py
@@ -1,4 +1,4 @@
-from typing import List  # NOQA
+from typing import List, Dict  # NOQA
 
 from n0core.processor import Processor
 from n0core.processor import IncompatibleMessage
@@ -10,14 +10,18 @@ from n0core.gateway import Gateway  # NOQA
 
 class Agent(Processor):
     def __init__(self,
-                 target,       # type: Target
-                 model_types,  # type: List[str]
-                 notification  # type: Gateway
+                 model_types,   # type: List[str]
+                 notification,  # type: Gateway
+                 target={}      # type: Dict[str, Target]
                  ):
         # type: (...) -> None
-        self.__target = target
         self.__model_types = model_types
         self.__notification = notification
+        self.__target = target
+
+    def add_target(self, type, target):
+        # type: (str, Target) -> None
+        self.__target[type] = target
 
     def proccess(self, message):
         # type: (Notification) -> None
@@ -28,7 +32,7 @@ class Agent(Processor):
         if not message.is_succeeded:
             raise IncompatibleMessage
 
-        model, is_succeeded, description = self.__target.apply(message.model)
+        model, is_succeeded, description = self.__target[message.model.type].apply(message.model)
         notification = Notification(spec_id=message.spec_id,
                                     model=model,
                                     event=Notification.EVENTS.APPLIED,
