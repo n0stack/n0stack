e15a7ac2f05792f535aabf3c2049c76e7dab46dc
h-otter <h-otter@outlook.jp>
2017-12-29 22:36:12 +0900

    add test

--- tests/target/network/test_flat.py
+++ tests/target/network/test_flat.py
@@ -7,16 +7,16 @@ INTERFACE_NAME = ""
 NETWORK_ID = "00f967af-0cc6-443e-b2a9-277b158f42f0"
 
 up_network = Network(NETWORK_ID, "flat", Network.STATES.UP, "test_network")
-up_network.add_subnet("192.168.0.0/24", "192.168.0.3-192.168.0.127", ["192.168.0.254"], "192.168.0.254")
+up_network.apply_subnet("192.168.1.0/24", "192.168.0.3-192.168.0.127", ["192.168.0.254"], "192.168.0.254")
 
 delete_network = Network(NETWORK_ID, "flat", Network.STATES.DELETED, "test_network", "nflat00f967af")
-delete_network.add_subnet("192.168.0.0/24", "192.168.0.3-192.168.0.127", ["192.168.0.254"], "192.168.0.254")
+delete_network.apply_subnet("192.168.1.0/24", "192.168.0.3-192.168.0.127", ["192.168.0.254"], "192.168.0.254")
 
 nic = NIC("755cedc3-1fe2-44a8-bc45-93d8edf2ad82",
           "dhcp",
           NIC.STATES.ATTACHED,
           "test_nic",
-          "1234567890abcd",
+          "e2:78:18:7e:da:4e",
           ["192.168.0.1"])
 nic.add_dependency(up_network, "n0stack/n0core/resource/port/network")
 
