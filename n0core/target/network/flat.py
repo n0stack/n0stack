7d5e36498b26f66b220d975f4f2de3605d96edce
h-otter <h-otter@outlook.jp>
2017-12-30 22:03:38 +0900

    separate dependenty without test referenced in #58

--- n0core/target/network/flat.py
+++ n0core/target/network/flat.py
@@ -5,7 +5,6 @@ from n0library.logger import Logger
 from n0core.target.network import Network as NetworkTarget
 from n0core.model import Model  # NOQA
 from n0core.model.resource.network import Network
-from n0core.model.resource.nic import NIC
 from n0core.lib.dhcp.dnsmasq import Dnsmasq
 
 
@@ -21,46 +20,28 @@ class Flat(NetworkTarget):
 
     def apply(self, model):
         # type: (Model) -> Tuple[Model, bool, str]
-        resource_type = model.type.split("/")[1]
+        model = cast(Network, model)
+        dnsmasq = Dnsmasq(model.id, self.bridge_name(model.id))
 
-        if resource_type == "network":
-            model = cast(Network, model)
-            dnsmasq = Dnsmasq(model.id, self.bridge_name(model.id))
+        if model.state == Network.STATES.UP:
+            self.apply_bridge(model.id)
+            model.bridge = self.bridge_name(model.id)
 
-            if model.state == Network.STATES.UP:
-                self.apply_bridge(model.id)
-                model.bridge = self.bridge_name(model.id)
+            subnet = model.subnets[0]  # first version support only one subnet
 
-                subnet = model.subnets[0]  # first version support only one subnet
+            if dnsmasq.get_pid():  # dhcp is running
+                dnsmasq.respawn_process(subnet.dhcp.range)
+            else:
+                dhcp_addr = ip_interface(subnet.cidr) + 1
+                dnsmasq.create_dhcp_server(dhcp_addr, subnet.dhcp.range)  # rangeが変わったらip addressも変えないといけないのでは？
 
-                if dnsmasq.get_pid():  # dhcp is running
-                    dnsmasq.respawn_process(subnet.dhcp.range)
-                else:
-                    dhcp_addr = ip_interface(subnet.cidr) + 1
-                    dnsmasq.create_dhcp_server(dhcp_addr, subnet.dhcp.range)  # rangeが変わったらip addressも変えないといけないのでは？
+        elif model.state == Network.STATES.DOWN:
+            self.apply_bridge(model.id, state="down")
+            dnsmasq.stop_process()
 
-            elif model.state == Network.STATES.DOWN:
-                self.apply_bridge(model.id, state="down")
-                dnsmasq.stop_process()
-
-            elif model.state == Network.STATES.DELETED:
-                dnsmasq.delete_dhcp_server()
-                self.delete_bridge(model.id)
-
-        elif resource_type == "nic":
-            model = cast(NIC, model)
-            network = cast(Network, model.depend_on("n0stack/n0core/resource/nic/network")[0].model)
-            dnsmasq = Dnsmasq(network.id, network.bridge)
-
-            if model.state == NIC.STATES.ATTACHED:
-                dnsmasq.add_allowed_host(model.hw_addr)
-
-                for i in model.ip_addrs:
-                    dnsmasq.add_host_entry(model.hw_addr, i)
-
-            elif model.state == NIC.STATES.DELETED:
-                dnsmasq.delete_host_entry(model.hw_addr)
-                dnsmasq.delete_allowed_host(model.hw_addr)
+        elif model.state == Network.STATES.DELETED:
+            dnsmasq.delete_dhcp_server()
+            self.delete_bridge(model.id)
 
         return model, True, "Succeeded"
 
