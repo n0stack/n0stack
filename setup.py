e044a64dc56b687fd2d197cb65ccc05447f7ecc9
h-otter <h-otter@outlook.jp>
2018-01-09 02:33:37 +0900

    update about setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,44 @@ if __name__ == "__main__":
     with open('README.md') as f:
         readme = f.read()
 
+    requires = [
+        # common use
+        "git+https://github.com/n0stack/n0library.git",
+
+        # n0core.model.network, n0core.model.nic
+        "netaddr",
+        "pyroute2",
+        "python-iptables",
+        "git+https://github.com/larsks/python-netns.git",
+
+        # n0core.target.vm
+        # "libvirt-python"
+    ]
+
+    extras = {
+        "test": [
+            "flake8",
+            "mypy",
+            "lxml",
+        ],
+        "docs": [
+            "Sphinx",
+            "sphinx_rtd_theme",
+            "recommonmark",
+            "commonmark",
+        ],
+    }
+
     setup(
         name='n0core',
-        version='0.0.0',
+        version='0.0.4',
         description='n0stack IaaS component',
         long_description=readme,
         url='https://github.com/n0stack/n0core',
         author='n0stack developer team',
-        packages=find_packages()
+        packages=find_packages(),
+        install_requires=list(filter(lambda p: "http" not in p, requires)),
+        dependency_links=list(filter(lambda p: "http" in p, requires)),
+        tests_requires=extras['test'],
+        extras_require=extras,
     )
