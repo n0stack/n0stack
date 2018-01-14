7aa37b3f2d30db653d2e10922f1e1711c5413792
h-otter <h-otter@outlook.jp>
2018-01-14 15:24:05 +0900

    add flat target without test, because explaining concept

---
+++ target/network/flat/flat.go
@@ -0,0 +1,100 @@
+package flat
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/satori/go.uuid"
+
+	"github.com/n0stack/n0core/model"
+	"github.com/vishvananda/netlink"
+)
+
+type Flat struct {
+	InterfaceName string
+}
+
+func (f Flat) Apply(m model.AbstractModel) (string, bool) {
+	n, ok := m.(*model.Network)
+	if !ok {
+		return "Failed to cast model to network.", false
+	}
+
+	switch n.State {
+	case "UP":
+		b, err := netlink.LinkByName(f.GetBridgeName(n.ID))
+		if err != nil {
+			err = nil
+
+			i, err := netlink.LinkByName(f.InterfaceName)
+			if err != nil {
+				return "Failed to get interface by name", false
+			}
+
+			ok := f.createBridge(i)
+			if !ok {
+				return "Failed to create bridge", false
+			}
+
+			b, err = netlink.LinkByName(f.GetBridgeName(n.ID))
+			if err != nil {
+				return "Failed to get bridge by name", false
+			}
+
+			n.Bridge = b.Attrs().Name
+		}
+
+	case "DOWN":
+		b, err := netlink.LinkByName(f.GetBridgeName(n.ID))
+		if err != nil {
+			err = nil
+
+			i, err := netlink.LinkByName(f.InterfaceName)
+			if err != nil {
+				return "Failed to get interface by name", false
+			}
+
+			ok := f.createBridge(i)
+			if !ok {
+				return "Failed to create bridge", false
+			}
+
+			b, err = netlink.LinkByName(f.GetBridgeName(n.ID))
+			if err != nil {
+				return "Failed to get bridge by name", false
+			}
+
+			n.Bridge = b.Attrs().Name
+		}
+		netlink.LinkSetDown(b)
+
+	case "DELETED":
+		b, err := netlink.LinkByName(f.GetBridgeName(n.ID))
+		if err != nil {
+			return "Failed to get bridge by name", false
+		}
+
+		netlink.LinkDel(b)
+	}
+
+	return "", true
+}
+
+func (f Flat) GetBridgeName(id uuid.UUID) string {
+	i := strings.Split(id.String(), "-")
+	return fmt.Sprintf("nbr%s", i[0])
+}
+
+func (f Flat) createBridge(i netlink.Link) bool {
+	la := netlink.NewLinkAttrs()
+	la.Name = ""
+	b := &netlink.Bridge{LinkAttrs: la}
+
+	err := netlink.LinkAdd(b)
+	if err != nil {
+		return false
+	}
+	netlink.LinkSetMaster(i, b)
+
+	return true
+}
