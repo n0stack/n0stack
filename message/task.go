49a7ea9043a00bc5366f1a23524ff4d53dfa3fb1
h-otter <h-otter@outlook.jp>
2018-01-19 01:44:39 +0900

    task.Models to task.Model

--- message/task.go
+++ message/task.go
@@ -10,8 +10,8 @@ import (
 type Task struct {
 	TaskID      uuid.UUID `yaml:"taskID"`
 	Task        string
-	Models      []model.AbstractModel // sliceにする必要があるかわからない
-	Annotations map[string]string     // 必要かどうかわからない
+	Model       model.AbstractModel
+	Annotations map[string]string // 必要かどうかわからない
 }
 
 func (t *Task) UnmarshalYAML(unmarshal func(interface{}) error) error {
@@ -31,18 +31,15 @@ func (t *Task) UnmarshalYAML(unmarshal func(interface{}) error) error {
 		return nil
 	}
 
-	mms, ok := mi.([]interface{})
+	mm, ok := mi.(map[interface{}]interface{})
 	if !ok {
 		return fmt.Errorf("Failed to parse model")
 	}
 
-	t.Models = make([]model.AbstractModel, len(mms))
-	for i, mm := range mms {
-		var err error
-		t.Models[i], err = model.MapToAbstractModel(mm.(map[interface{}]interface{}))
-		if err != nil {
-			return err
-		}
+	var err error
+	t.Model, err = model.MapToAbstractModel(mm)
+	if err != nil {
+		return err
 	}
 
 	return nil
