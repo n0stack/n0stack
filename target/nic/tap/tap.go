5e14e0394a5577d2e90099589fac62aff80de369
h-otter <h-otter@outlook.jp>
2018-03-19 02:03:39 +0900

    redesign all

--- target/nic/tap/tap.go
+++
@@ -1,8 +0,0 @@
-package tap
-
-// ip tuntap add dev $tap_name mode tap # user group
-// ip link set master $bridge_name dev $tap_name
-// ip link set up dev $tap_name
-// iptables -P FORWARD ACCEPT
-
-const NICType = "tap"
