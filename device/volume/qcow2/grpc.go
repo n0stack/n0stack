bc7455d7d951e3cf2d6558a21079b465d3499347
h-otter <h-otter@outlook.jp>
2018-03-21 00:10:41 +0900

    add qcow2 volume type

---
+++ device/volume/qcow2/grpc.go
@@ -0,0 +1,70 @@
+package qcow2
+
+import (
+	"context"
+
+	"github.com/n0stack/n0core/lib"
+	"github.com/n0stack/proto/device/volume"
+
+	n0stack "github.com/n0stack/proto"
+)
+
+const modelType = "device/volume/qcow2"
+
+type Agent struct{}
+
+func (a Agent) Apply(ctx context.Context, spec *volume.Spec) (*volume.Response, error) {
+	var (
+		n *n0stack.Notification
+		q qcow2
+	)
+	v := &volume.Response{
+		Notification: n,
+		Status:       &q.Status,
+	}
+
+	if n = q.getStatus(spec.Device.Model); !n.Success {
+		return v, nil
+	}
+
+	if q.Storage == nil {
+		if n = q.createImage(spec.Storage.Bytes); !n.Success {
+			return v, nil
+		}
+
+		if n = q.getStatus(spec.Device.Model); !n.Success {
+			return v, nil
+		}
+	} else {
+		if n = q.resizeImage(spec.Storage.Bytes); !n.Success {
+			return v, nil
+		}
+	}
+
+	v.Notification = lib.MakeNotification("Apply", true, "")
+	return v, nil
+}
+
+func (a Agent) Delete(ctx context.Context, model *n0stack.Model) (*volume.Response, error) {
+	var (
+		n *n0stack.Notification
+		q qcow2
+	)
+	v := &volume.Response{
+		Notification: n,
+		Status:       &q.Status,
+	}
+
+	if n = q.getStatus(model); !n.Success {
+		return v, nil
+	}
+
+	if q.Storage != nil {
+		if n = q.deleteImage(); !n.Success {
+			return v, nil
+		}
+	}
+
+	v.Notification = lib.MakeNotification("Delete", true, "")
+	return v, nil
+}
