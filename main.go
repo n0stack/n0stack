2183bbe2ec34b3b31823579afe287d61b2119992
h-otter <h-otter@outlook.jp>
2018-04-08 23:37:25 +0900

    use env vars

--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"net"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/n0stack/go.proto/kvm/v0"
 
 	pqcow2 "github.com/n0stack/go.proto/qcow2/v0"
 	ptap "github.com/n0stack/go.proto/tap/v0"
 	"github.com/n0stack/n0core/kvm"
+	"github.com/n0stack/n0core/qcow2"
 	"github.com/n0stack/n0core/tap"
-	"github.com/n0stack/n0core/volume/qcow2"
 
 	_ "github.com/mattn/go-sqlite3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
+type Environment struct {
+	Starter               string `default:""`
+	BindPort              string `default:":20180"`
+	OutboundInterfaceName string `required:"true"`
+}
+
+var (
+	Env Environment
+)
+
 func main() {
-	lis, err := net.Listen("tcp", ":20180")
+	lis, err := net.Listen("tcp", Env.BindPort)
 	if err != nil {
 		panic(err)
 	}
@@ -34,10 +45,20 @@ func main() {
 
 	pkvm.RegisterKVMServiceServer(s, &kvm.Agent{})
 	pqcow2.RegisterQcow2ServiceServer(s, &qcow2.Agent{})
-	ptap.RegisterTapServiceServer(s, &tap.Agent{InterfaceName: "enp0s25"}) // 環境変数から取る
+	ptap.RegisterTapServiceServer(s, &tap.Agent{InterfaceName: Env.OutboundInterfaceName})
 
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		panic(err)
+	}
+}
+
+func init() {
+	// packageがインストールされているか確認する
+
+	// env vars
+	err := envconfig.Process("n0core", &Env)
+	if err != nil {
+		panic("Failed to parse environment variables: " + err.Error())
 	}
 }
