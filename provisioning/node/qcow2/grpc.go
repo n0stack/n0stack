5f36ca2bb9e9a4cbe99c04becf1cf9cccc46b570
yatuhashi kei <keyansuiya@gmail.com>
2018-06-17 08:33:18 +0900

    ADD volume/DownloadQcow2

--- provisioning/node/qcow2/grpc.go
+++ provisioning/node/qcow2/grpc.go
@@ -2,10 +2,13 @@ package qcow2
 
 import (
 	fmt "fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 
 	"code.cloudfoundry.org/bytefmt"
@@ -70,8 +73,39 @@ func (a Qcow2Agent) ApplyQcow2(ctx context.Context, req *ApplyQcow2Request) (*Qc
 	return req.Qcow2, nil
 }
 
-func (a Qcow2Agent) DownloadQcow2(context.Context, *DownloadQcow2Request) (*Qcow2, error) {
-	return nil, nil
+func (a Qcow2Agent) DownloadQcow2(ctx context.Context, req *DownloadQcow2Request) (*Qcow2, error) {
+	u, err := url.Parse(req.Qcow2.Url)
+	if err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Failed to parse url, err:%v.", err.Error())
+	}
+
+	if a.qcow2IsExist(u) {
+		// need to implement resizing.
+		return req.Qcow2, grpc.Errorf(codes.AlreadyExists, "")
+	}
+
+	// Download qcow2 img
+	response, err := http.Get(req.SourceUrl)
+	if err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Failed to parse SourceUrl, err:%v.", err.Error())
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Failed to download img, err:%v.", err.Error())
+	}
+	bytes := len(body)
+	_, filename := path.Split(req.SourceUrl)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0666)
+	defer file.Close()
+	if err != nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Failed to save img, err:%v.", err.Error())
+	}
+	file.Write(body)
+	if err := a.createQcow2(bytes, u); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Failed to create qcow2, err:%v.", err.Error())
+	}
+	log.Printf("[INFO] Applied qcow2, qcow2:%v", req.Qcow2)
+	return req.Qcow2, nil
 }
 
 func (a Qcow2Agent) BuildQcow2WithPacker(context.Context, *BuildQcow2WithPackerRequest) (*Qcow2, error) {
