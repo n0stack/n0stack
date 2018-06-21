a73fd4670960a4eda5901f19e1bce6d6b5ade4a9
h-otter <h-otter@outlook.jp>
2018-06-21 18:00:18 +0900

    worked compute without mounting volume

--- provisioning/compute/api.go
+++ provisioning/compute/api.go
@@ -3,6 +3,15 @@ package compute
 import (
 	"context"
 	"log"
+	"math/rand"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/n0stack/n0core/provisioning/node"
+	"github.com/n0stack/n0core/provisioning/node/iproute2"
+	"github.com/n0stack/n0core/provisioning/node/kvm"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes/empty"
@@ -12,13 +21,27 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
+const (
+	VMNamespace                = "6565cf73-9845-49d7-bdd7-76970e5ebe4f"
+	AnnotationNodeName         = "n0core/node_name"
+	AnnotationQmpPath          = "n0core/qmp_path"
+	AnnotationVncWebsocketPort = "n0core/vnc_websocket_port"
+
+	ApplyIproute2Deadline = time.Second * 10
+	ApplyKVMDeadline      = time.Second * 10
+)
+
 type ComputeAPI struct {
-	dataStore datastore.Datastore
+	dataStore       datastore.Datastore
+	nodeConnections *node.NodeConnections
+	qmpBaseDir      string
 }
 
-func CreateComputeAPI(ds datastore.Datastore) (*ComputeAPI, error) {
+func CreateComputeAPI(ds datastore.Datastore, nc *node.NodeConnections, qmpBaseDir string) (*ComputeAPI, error) {
 	return &ComputeAPI{
-		dataStore: ds,
+		dataStore:       ds,
+		nodeConnections: nc,
+		qmpBaseDir:      qmpBaseDir,
 	}, nil
 }
 
@@ -61,6 +84,19 @@ func (a ComputeAPI) GetCompute(ctx context.Context, req *pprovisioning.GetComput
 	return res, nil
 }
 
+func (a ComputeAPI) genQmpPath(name string) string {
+	return filepath.Join(a.qmpBaseDir, name, "monitor.sock")
+}
+
+// well known portと /proc/sys/net/ipv4/ip_local_port_range を避ける
+// TODO: overflowしないか心配
+func (a ComputeAPI) genVncWebsocketPort() uint32 {
+	const LinuxIPLocalPortMin = 32768
+	const LinuxIPLocalPortMax = 60999
+
+	return rand.Uint32()%(LinuxIPLocalPortMax-LinuxIPLocalPortMin) + LinuxIPLocalPortMin
+}
+
 func (a ComputeAPI) ApplyCompute(ctx context.Context, req *pprovisioning.ApplyComputeRequest) (*pprovisioning.Compute, error) {
 	prev := &pprovisioning.Compute{}
 	err := a.dataStore.Get(req.Metadata.Name, prev)
@@ -68,10 +104,10 @@ func (a ComputeAPI) ApplyCompute(ctx context.Context, req *pprovisioning.ApplyCo
 		return nil, grpc.Errorf(codes.Internal, "Failed to get db, got:%v.", err.Error())
 	}
 	if prev.Metadata == nil && req.Metadata.Version != 0 {
-		return nil, grpc.Errorf(codes.InvalidArgument, "Set the same version as GetVolume result, have:%d, want:0.", req.Metadata.Version)
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set the same version as Get result, have:%d, want:0.", req.Metadata.Version)
 	}
 	if prev.Metadata != nil && req.Metadata.Version != prev.Metadata.Version {
-		return nil, grpc.Errorf(codes.InvalidArgument, "Set the same version as GetVolume result, have:%d, want:%d.", req.Metadata.Version, prev.Metadata.Version)
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set the same version as Get result, have:%d, want:%d.", req.Metadata.Version, prev.Metadata.Version)
 	}
 
 	res := &pprovisioning.Compute{
@@ -84,8 +120,105 @@ func (a ComputeAPI) ApplyCompute(ctx context.Context, req *pprovisioning.ApplyCo
 		res.Status = prev.Status
 	}
 
-	res.Metadata.Version++
+	nn, ok := req.Metadata.Annotations[AnnotationNodeName]
+	if !ok {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set '%s'", AnnotationNodeName)
+	}
 
+	conn, err := a.nodeConnections.GetConnection(nn)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Fail to dial to node, err:%v node_name:%s", err.Error(), nn)
+	}
+	if conn == nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set valid '%s'", AnnotationNodeName) // TODO: エラーメッセージを何とかする
+	}
+	defer conn.Close()
+
+	// volume
+	// get volume
+	// use url
+	// set component as used
+
+	// iproute2
+	ipCli := iproute2.NewIproute2ServiceClient(conn)
+	for l, n := range req.Spec.Nics {
+		ctx, cancel := context.WithTimeout(context.Background(), ApplyIproute2Deadline)
+		defer cancel()
+
+		reqTap := &iproute2.ApplyTapRequest{
+			Tap: &iproute2.Tap{
+				Name:       l,
+				BridgeName: n.NetworkName, // TODO: 多分長さ制限で引っかかったりするので何か関数などでラップする必要がある
+				Type:       iproute2.Tap_FLAT,
+			},
+		}
+
+		if resTap, err := ipCli.ApplyTap(ctx, reqTap); err != nil {
+			return nil, grpc.Errorf(codes.Internal, "Failed to ApplyTap, err:'%s', req:'%s', res'%s'", err.Error(), reqTap, resTap)
+		}
+	}
+
+	// kvm
+	ns, _ := uuid.FromString(VMNamespace)
+	vmID := uuid.NewV5(ns, req.Metadata.Name)
+
+	qp, ok := res.Metadata.Annotations[AnnotationQmpPath]
+	if !ok {
+		qp = a.genQmpPath(req.Metadata.Name)
+	}
+
+	svwp, ok := res.Metadata.Annotations[AnnotationVncWebsocketPort]
+	var vwp uint32
+	if ok {
+		vwp64, err := strconv.ParseInt(svwp, 10, 32)
+		if err != nil {
+			return nil, grpc.Errorf(codes.InvalidArgument, "Set unsigned integer to '%s', err:'%s', have:'%s'", AnnotationVncWebsocketPort, err.Error(), svwp)
+		}
+
+		vwp = uint32(vwp64)
+	} else {
+		vwp = a.genVncWebsocketPort()
+	}
+
+	reqKVM := &kvm.KVM{
+		Name: req.Metadata.Name,
+		// generate from name
+		Uuid:             vmID.String(),
+		CpuCores:         req.Spec.Vcpus,
+		MemoryBytes:      req.Spec.MemoryBytes,
+		Nics:             map[string]*kvm.KVM_NIC{},
+		Storages:         map[string]*kvm.KVM_Storage{},
+		QmpPath:          qp,
+		VncWebsocketPort: vwp,
+	}
+	for l, n := range req.Spec.Nics {
+		reqKVM.Nics[l] = &kvm.KVM_NIC{
+			TapName: l,
+			HwAddr:  n.HardwareAddress,
+		}
+	}
+	for l, v := range req.Spec.Volumes {
+		reqKVM.Storages[l] = &kvm.KVM_Storage{
+			Url: v.VolumeName,
+
+			// とりあえずvolumeがひとつの場合のみをサポート
+			BootIndex: 1,
+		}
+	}
+
+	kvmCli := kvm.NewKVMServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), ApplyKVMDeadline)
+	defer cancel()
+
+	resKVM, err := kvmCli.ApplyKVM(ctx, &kvm.ApplyKVMRequest{Kvm: reqKVM})
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Failed to apply kvm, err:'%s', request: '%s', response: '%s'", err.Error(), reqKVM, resKVM)
+	}
+
+	res.Metadata.Annotations[AnnotationQmpPath] = resKVM.QmpPath
+	res.Metadata.Annotations[AnnotationVncWebsocketPort] = strconv.FormatUint(uint64(resKVM.VncWebsocketPort), 10)
+
+	res.Metadata.Version++
 	if err := a.dataStore.Apply(req.Metadata.Name, res); err != nil {
 		return nil, grpc.Errorf(codes.Internal, "Failed to apply for db, got:%v.", err.Error())
 	}
@@ -95,6 +228,46 @@ func (a ComputeAPI) ApplyCompute(ctx context.Context, req *pprovisioning.ApplyCo
 }
 
 func (a ComputeAPI) DeleteCompute(ctx context.Context, req *pprovisioning.DeleteComputeRequest) (*empty.Empty, error) {
+	prev := &pprovisioning.Compute{}
+	err := a.dataStore.Get(req.Name, prev)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Failed to get db, got:%v.", err.Error())
+	}
+
+	nn, ok := prev.Metadata.Annotations[AnnotationNodeName]
+	if !ok {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set '%s'", AnnotationNodeName)
+	}
+
+	conn, err := a.nodeConnections.GetConnection(nn)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Fail to dial to node, err:%v node_name:%s", err.Error(), nn)
+	}
+	if conn == nil {
+		return nil, grpc.Errorf(codes.InvalidArgument, "Set valid '%s'", AnnotationNodeName) // TODO: エラーメッセージを何とかする
+	}
+	defer conn.Close()
+
+	// kvm
+	kvmCli := kvm.NewKVMServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), ApplyKVMDeadline)
+	defer cancel()
+
+	if _, err := kvmCli.DeleteKVM(ctx, &kvm.DeleteKVMRequest{Name: req.Name}); err != nil {
+		return nil, grpc.Errorf(codes.Internal, "Failed to apply kvm, err:'%s', name: '%s'", err.Error(), req.Name)
+	}
+
+	// iproute2
+	ipCli := iproute2.NewIproute2ServiceClient(conn)
+	for l := range prev.Spec.Nics {
+		ctx, cancel := context.WithTimeout(context.Background(), ApplyIproute2Deadline)
+		defer cancel()
+
+		if _, err := ipCli.DeleteTap(ctx, &iproute2.DeleteTapRequest{Name: l}); err != nil {
+			return nil, grpc.Errorf(codes.Internal, "Failed to DeleteTap, err:'%s', name:'%s'", err.Error(), l)
+		}
+	}
+
 	d, err := a.dataStore.Delete(req.Name)
 	if err != nil {
 		return &empty.Empty{}, grpc.Errorf(codes.Internal, "message:Failed to delete from db.\tgot:%v", err.Error())
@@ -107,29 +280,29 @@ func (a ComputeAPI) DeleteCompute(ctx context.Context, req *pprovisioning.Delete
 }
 
 func (a ComputeAPI) WatchCompute(req *pprovisioning.WatchComputesRequest, res pprovisioning.ComputeService_WatchComputeServer) error {
-	return nil
+	return grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) Boot(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) Reboot(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) HardReboot(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) Shutdown(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) HardShutdown(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
 
 func (a ComputeAPI) Save(ctx context.Context, req *pprovisioning.ActionComputeRequest) (*pprovisioning.Compute, error) {
-	return nil, nil
+	return nil, grpc.Errorf(codes.Unimplemented, "")
 }
--- provisioning/node/kvm/README.md
+++ provisioning/node/kvm/README.md
@@ -8,15 +8,15 @@
 ```sh
 grpc_cli call localhost:20181 n0stack.n0core.kvm.KVMService/ApplyKVM \
 'kvm {
-  uuid: "9f8f7a4e-d314-4135-bebc-e0a44e7bcbe9"
-  name: "test-vm"
+  uuid: "a425dede-b6b3-5572-a3e2-7de689a6d8a5"
+  name: "test-compute"
   vnc_websocket_port: 5000
   qmp_path: "/tmp/monitor.sock"
 
   cpu_cores: 1
   memory_bytes: 1073741824
 
-  volumes: {
+  storages: {
     key: "test-volume"
     value: {
       url: "file:///tmp/ubuntu.qcow2"
