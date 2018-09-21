ab19153764861dd34066ebce7590bc43b128fc04
h-otter <h-otter@outlook.jp>
2018-09-22 05:06:44 +0900

    add ScheduleNewIPv4 on Network

---
+++ pkg/api/pool/network/resource.go
@@ -0,0 +1,59 @@
+package network
+
+import (
+	"encoding/hex"
+	"fmt"
+	"hash/crc32"
+	"net"
+
+	"github.com/n0stack/proto.go/resource/v0"
+)
+
+func GenerateHardwareAddress(id string) net.HardwareAddr {
+	cs := crc32.Checksum([]byte(id), crc32.IEEETable)
+	b, _ := hex.DecodeString(fmt.Sprintf("5254%08x", cs))
+	return net.HardwareAddr(b)
+}
+
+func CheckIPv4OnCIDR(request net.IP, cidr *net.IPNet) error {
+	if !cidr.Contains(request) {
+		return fmt.Errorf("Requested IPv4 '%s' is over from Network CIDR '%s'", request.String(), cidr.String())
+	}
+
+	return nil
+}
+
+func CheckConflictIPv4(request net.IP, reserved map[string]*presource.NetworkInterface) error {
+	for k, v := range reserved {
+		if request.String() == v.Ipv4Address {
+			return fmt.Errorf("Network interface '%s' is already have IPv4 address '%s'", k, request.String())
+		}
+	}
+
+	return nil
+}
+
+// O(len(reserved) ^ 2) なので要修正
+func ScheduleNewIPv4(cidr *net.IPNet, reserved map[string]*presource.NetworkInterface) net.IP {
+	// escape network address and broadcast address
+	for ip := NextIP(cidr.IP); cidr.Contains(NextIP(ip)); ip = NextIP(ip) {
+		if err := CheckConflictIPv4(ip, reserved); err == nil {
+			return ip
+		}
+	}
+
+	return nil
+}
+
+func NextIP(ip net.IP) net.IP {
+	res := net.ParseIP(ip.String())
+	for i := len(res) - 1; i >= 0; i-- {
+		res[i]++
+
+		if res[i] > 0 {
+			break
+		}
+	}
+
+	return res
+}
---
+++ pkg/api/pool/network/resource_test.go
@@ -0,0 +1,58 @@
+package network
+
+import (
+	"net"
+	"testing"
+
+	"github.com/n0stack/proto.go/resource/v0"
+)
+
+func TestGenerateHardwareAddress(t *testing.T) {
+	result := "52:54:f5:8c:a4:f2"
+	hw := GenerateHardwareAddress("hogehoge")
+	if hw.String() != result {
+		t.Errorf("Wrong hardware address\n\thave:%s\n\twant:%s", hw, result)
+	}
+}
+
+func TestScheduleNewIPv4(t *testing.T) {
+	_, cidr, _ := net.ParseCIDR("192.168.0.0/30")
+
+	cases := []struct {
+		// cidr *net.IPNet
+		reserved map[string]*presource.NetworkInterface
+		result   net.IP
+	}{
+		{
+			map[string]*presource.NetworkInterface{
+				"hoge": &presource.NetworkInterface{
+					Ipv4Address: "192.168.0.1",
+				},
+			},
+			net.ParseIP("192.168.0.2"),
+		},
+		{
+			map[string]*presource.NetworkInterface{
+				"foo": &presource.NetworkInterface{
+					Ipv4Address: "192.168.0.1",
+				},
+				"bar": &presource.NetworkInterface{
+					Ipv4Address: "192.168.0.2",
+				},
+			},
+			nil,
+		},
+	}
+
+	for _, c := range cases {
+		ip := ScheduleNewIPv4(cidr, c.reserved)
+
+		if c.result != nil && ip == nil {
+			t.Errorf("Wrong generated IPv4 address\n\thave:nil\n\twant:%s", c.result)
+		} else if c.result == nil && ip != nil {
+			t.Errorf("Wrong generated IPv4 address\n\thave:%s\n\twant:nil", ip)
+		} else if !ip.Equal(c.result) {
+			t.Errorf("Wrong generated IPv4 address\n\thave:%s\n\twant:%s", ip, c.result)
+		}
+	}
+}
