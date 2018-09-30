772731102d0eeb5050559749d1b8472e155c9eba
h-otter <h-otter@outlook.jp>
2018-09-30 16:16:08 +0900

    move provisioning api method

--- pkg/api/provisioning/block_storage_api.go
+++ pkg/api/provisioning/block_storage_api.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"reflect"
 
+	"github.com/n0stack/proto.go/budget/v0"
 	"github.com/n0stack/proto.go/pool/v0"
 	"github.com/n0stack/proto.go/provisioning/v0"
 
@@ -376,4 +377,32 @@ func (a *BlockStorageAPI) ProtectBlockStorage(ctx context.Context, req *pprovisi
 }
 func (a *BlockStorageAPI) UnprotectBlockStorage(ctx context.Context, req *pprovisioning.UnprotectBlockStorageRequest) (*pprovisioning.BlockStorage, error) {
 	return nil, grpc.Errorf(codes.Unimplemented, "")
+}
+
+func (a BlockStorageAPI) reserveStorage(name string, annotations map[string]string, req, limit uint64) (string, string, error) {
+	var rcr *ppool.ReserveStorageResponse
+	var err error
+	if node, ok := annotations[AnnotationRequestNodeName]; !ok {
+		rcr, err = a.nodeAPI.ScheduleStorage(context.Background(), &ppool.ScheduleStorageRequest{
+			StorageName: name,
+			Storage: &pbudget.Storage{
+				RequestBytes: req,
+				LimitBytes:   limit,
+			},
+		})
+	} else {
+		rcr, err = a.nodeAPI.ReserveStorage(context.Background(), &ppool.ReserveStorageRequest{
+			Name:        node,
+			StorageName: name,
+			Storage: &pbudget.Storage{
+				RequestBytes: req,
+				LimitBytes:   limit,
+			},
+		})
+	}
+	if err != nil {
+		return "", "", err // TODO: #89
+	}
+
+	return rcr.Name, rcr.StorageName, nil
 }
