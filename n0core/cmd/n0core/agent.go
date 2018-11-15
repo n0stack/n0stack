54816436ac26df23844d16990c222f2d8f5671bb
h-otter <h-otter@outlook.jp>
2018-11-15 19:46:51 +0900

    add file http endpoint to migrate other node, bug there are security problem arise from no authentication

--- n0core/cmd/n0core/agent.go
+++ n0core/cmd/n0core/agent.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/n0stack/n0stack/n0core/pkg/api/pool/node"
 	"github.com/n0stack/n0stack/n0core/pkg/api/provisioning"
 	"github.com/urfave/cli"
@@ -15,20 +19,34 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-func ServeAgent(ctx *cli.Context) error {
-	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", ctx.String("bind-address"), ctx.Int("bind-port")))
-	if err != nil {
-		return err
-	}
+// References:
+// 	https://github.com/grpc/grpc-go/pull/1406/files#diff-34c6b408d72845d076d47126c29948d1R591
+// 	https://qiita.com/ktateish/items/ff5c045e3cebc59bf119
+func MixHandler(grpcServer *grpc.Server, httpHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			httpHandler.ServeHTTP(w, r)
+		}
+	})
+}
 
-	bvm := filepath.Join(ctx.String("base-directory"), "virtual_machine")
+func ServeAgent(ctx *cli.Context) error {
+	bind := fmt.Sprintf("%s:%d", ctx.String("bind-address"), ctx.Int("bind-port"))
+	baseDirectory := ctx.String("base-directory")
+	nodeName := ctx.String("name")
+	advertiseAddress := ctx.String("advertise-address")
+	nodeAPI := ctx.String("node-api-endpoint")
+
+	bvm := filepath.Join(baseDirectory, "virtual_machine")
 	vma, err := provisioning.CreateVirtualMachineAgentAPI(bvm)
 	if err != nil {
 		return err
 	}
 
-	bv := filepath.Join(ctx.String("base-directory"), "block_storage")
-	va, err := provisioning.CreateBlockStorageAgentAPI(bv)
+	bbs := filepath.Join(baseDirectory, "block_storage")
+	va, err := provisioning.CreateBlockStorageAgentAPI(bbs)
 	if err != nil {
 		return err
 	}
@@ -51,15 +69,28 @@ func ServeAgent(ctx *cli.Context) error {
 			// grpc_zap.UnaryServerInterceptor(zapLogger),
 			// grpc_auth.UnaryServerInterceptor(auth),
 			// grpc_prometheus.UnaryServerInterceptor,
-		)))
+		)),
+	)
 	provisioning.RegisterBlockStorageAgentServiceServer(grpcServer, va)
 	provisioning.RegisterVirtualMachineAgentServiceServer(grpcServer, vma)
 	reflection.Register(grpcServer)
 
-	if err := node.RegisterNodeToAPI(ctx.String("name"), ctx.String("advertise-address"), ctx.String("node-api-endpoint")); err != nil {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Static("/api/block_storage/files", bbs)
+
+	if err := node.RegisterNodeToAPI(nodeName, advertiseAddress, nodeAPI); err != nil {
+		return err
+	}
+
+	lis, err := net.Listen("tcp", bind)
+	if err != nil {
 		return err
 	}
 
 	log.Printf("[INFO] Started API: version=%s", version)
+	// TODO: セキュリティ的に問題あり 暫定処置
+	go e.Start("0.0.0.0:8081")
 	return grpcServer.Serve(lis)
 }
