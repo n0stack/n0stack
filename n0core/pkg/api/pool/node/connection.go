b1d5944098d2a74c89dbb348dfba4f9b0491a91f
h-otter <h-otter@outlook.jp>
2018-12-02 21:32:30 +0900

    fix small test

--- n0core/pkg/api/pool/node/connection.go
+++ n0core/pkg/api/pool/node/connection.go
@@ -62,3 +62,22 @@ func (nc NodeConnections) GetConnection(nodeName string) (*grpc.ClientConn, erro
 
 	return conn, nil
 }
+
+func GetConnection(ctx context.Context, api ppool.NodeServiceClient, nodeName string) (*grpc.ClientConn, error) {
+	n, err := api.GetNode(ctx, &ppool.GetNodeRequest{Name: nodeName})
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get node from API")
+	}
+
+	if n.State != ppool.Node_Ready {
+		return nil, nil
+	}
+
+	// port を何かから取れるようにする
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", n.Address, 20181), grpc.WithInsecure())
+	if err != nil {
+		return nil, errors.Wrap(err, "Fail to dial to node")
+	}
+
+	return conn, nil
+}
