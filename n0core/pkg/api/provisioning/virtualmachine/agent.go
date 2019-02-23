1dbc67442c3dce6937cce3ddac1768aaaa5f6438
h-otter <h-otter@outlook.jp>
2019-02-24 00:35:34 +0900

    fix failed to boot virtual machine which is shutted down by guest os operation

--- n0core/pkg/api/provisioning/virtualmachine/agent.go
+++ n0core/pkg/api/provisioning/virtualmachine/agent.go
@@ -18,9 +18,9 @@ import (
 	"github.com/n0stack/n0stack/n0core/pkg/driver/cloudinit/configdrive"
 	"github.com/n0stack/n0stack/n0core/pkg/driver/iproute2"
 	"github.com/n0stack/n0stack/n0core/pkg/driver/qemu"
-	"github.com/n0stack/n0stack/n0core/pkg/driver/qemu_img"
-	"github.com/n0stack/n0stack/n0core/pkg/util/grpc"
-	"github.com/n0stack/n0stack/n0core/pkg/util/net"
+	img "github.com/n0stack/n0stack/n0core/pkg/driver/qemu_img"
+	grpcutil "github.com/n0stack/n0stack/n0core/pkg/util/grpc"
+	netutil "github.com/n0stack/n0stack/n0core/pkg/util/net"
 	"github.com/n0stack/n0stack/n0proto.go/pkg/transaction"
 )
 
@@ -98,151 +98,150 @@ func (a VirtualMachineAgent) BootVirtualMachine(ctx context.Context, req *BootVi
 	if err != nil {
 		return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to open qemu process: %s", err.Error())
 	}
-	if q.IsRunning() {
-		return nil, grpcutil.WrapGrpcErrorf(codes.AlreadyExists, "Qemu process is already running")
-	}
-
-	if err := q.Start(id, filepath.Join(wd, QmpMonitorSocketFile), vcpus, mem); err != nil {
-		return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to start qemu process: err=%s", err.Error())
-	}
 	defer q.Close()
-	tx.PushRollback("delete Qemu", func() error {
-		return q.Delete()
-	})
 
-	eth := make([]*configdrive.CloudConfigEthernet, len(req.Netdevs))
-	{
-		for i, nd := range req.Netdevs {
-			b, err := iproute2.NewBridge(netutil.StructLinuxNetdevName(nd.NetworkName))
-			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to create bridge '%s': err='%s'", nd.NetworkName, err.Error())
-			}
-			tx.PushRollback("delete created bridge", func() error {
-				links, err := b.ListSlaves()
-				if err != nil {
-					return fmt.Errorf("Failed to list links of bridge '%s': err='%s'", nd.NetworkName, err.Error())
-				}
-
-				// TODO: 以下遅い気がする
-				i := 0
-				for _, l := range links {
-					if _, err := iproute2.NewTap(l); err == nil {
-						i++
-					}
-				}
-				if i == 0 {
-					if err := b.Delete(); err != nil {
-						return fmt.Errorf("Failed to delete bridge '%s': err='%s'", b.Name(), err.Error())
-					}
-				}
-
-				return nil
-			})
-
-			t, err := iproute2.NewTap(netutil.StructLinuxNetdevName(nd.Name))
-			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to create tap '%s': err='%s'", nd.Name, err.Error())
-			}
-			tx.PushRollback("delete created tap", func() error {
-				if err := t.Delete(); err != nil {
-					return fmt.Errorf("Failed to delete tap '%s': err='%s'", nd.Name, err.Error())
-				}
-
-				return nil
-			})
-			if err := t.SetMaster(b); err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to set master of tap '%s' as '%s': err='%s'", t.Name(), b.Name(), err.Error())
-			}
-
-			hw, err := net.ParseMAC(nd.HardwareAddress)
-			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Hardware address '%s' is invalid on netdev '%s'", nd.HardwareAddress, nd.Name)
-			}
-			if err := q.AttachTap(nd.Name, t.Name(), hw); err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach tap: err='%s'", err.Error())
-			}
-
-			// Cloudinit settings
-			eth[i] = &configdrive.CloudConfigEthernet{
-				MacAddress: hw,
-			}
-
-			if nd.Ipv4AddressCidr != "" {
-				ip := netutil.ParseCIDR(nd.Ipv4AddressCidr)
-				if ip == nil {
-					return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "Set valid ipv4_address_cidr: value='%s'", nd.Ipv4AddressCidr)
-				}
-				nameservers := make([]net.IP, len(nd.Nameservers))
-				for i, n := range nd.Nameservers {
-					nameservers[i] = net.ParseIP(n)
-				}
-
-				eth[i].Address4 = ip
-				eth[i].Gateway4 = net.ParseIP(nd.Ipv4Gateway)
-				eth[i].NameServers = nameservers
-
-				// Gateway settings
-				if nd.Ipv4Gateway != "" {
-					mask := ip.SubnetMaskBits()
-					gatewayIP := fmt.Sprintf("%s/%d", nd.Ipv4Gateway, mask)
-					if err := b.SetAddress(gatewayIP); err != nil {
-						return nil, grpcutil.WrapGrpcErrorf(codes.Internal, errors.Wrapf(err, "Failed to set gateway IP to bridge: value=%s", gatewayIP).Error())
-					}
-				}
-			}
+	if !q.IsRunning() {
+		if err := q.Start(id, filepath.Join(wd, QmpMonitorSocketFile), vcpus, mem); err != nil {
+			return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to start qemu process: err=%s", err.Error())
 		}
-	}
-
-	{
-		parsedKeys := make([]ssh.PublicKey, len(req.SshAuthorizedKeys))
-		for i, k := range req.SshAuthorizedKeys {
-			parsedKeys[i], _, _, _, err = ssh.ParseAuthorizedKey([]byte(k))
-			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "ssh_authorized_keys is invalid: value='%s', err='%s'", k, err.Error())
-			}
-		}
-
-		c := configdrive.StructConfig(req.LoginUsername, req.Name, parsedKeys, eth)
-		p, err := c.Generate(wd)
-		if err != nil {
-			return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to generate cloudinit configdrive:  err='%s'", err.Error())
-		}
-		req.Blockdevs = append(req.Blockdevs, &BlockDev{
-			Name: "configdrive",
-			Url: (&url.URL{
-				Scheme: "file",
-				Path:   p,
-			}).String(),
-			BootIndex: 50, // MEMO: 適当
+		tx.PushRollback("delete Qemu", func() error {
+			return q.Delete()
 		})
-	}
 
-	{
-		for _, bd := range req.Blockdevs {
-			u, err := url.Parse(bd.Url)
-			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "url '%s' is invalid url: '%s'", bd.Url, err.Error())
+		eth := make([]*configdrive.CloudConfigEthernet, len(req.Netdevs))
+		{
+			for i, nd := range req.Netdevs {
+				b, err := iproute2.NewBridge(netutil.StructLinuxNetdevName(nd.NetworkName))
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to create bridge '%s': err='%s'", nd.NetworkName, err.Error())
+				}
+				tx.PushRollback("delete created bridge", func() error {
+					links, err := b.ListSlaves()
+					if err != nil {
+						return fmt.Errorf("Failed to list links of bridge '%s': err='%s'", nd.NetworkName, err.Error())
+					}
+
+					// TODO: 以下遅い気がする
+					i := 0
+					for _, l := range links {
+						if _, err := iproute2.NewTap(l); err == nil {
+							i++
+						}
+					}
+					if i == 0 {
+						if err := b.Delete(); err != nil {
+							return fmt.Errorf("Failed to delete bridge '%s': err='%s'", b.Name(), err.Error())
+						}
+					}
+
+					return nil
+				})
+
+				t, err := iproute2.NewTap(netutil.StructLinuxNetdevName(nd.Name))
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to create tap '%s': err='%s'", nd.Name, err.Error())
+				}
+				tx.PushRollback("delete created tap", func() error {
+					if err := t.Delete(); err != nil {
+						return fmt.Errorf("Failed to delete tap '%s': err='%s'", nd.Name, err.Error())
+					}
+
+					return nil
+				})
+				if err := t.SetMaster(b); err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to set master of tap '%s' as '%s': err='%s'", t.Name(), b.Name(), err.Error())
+				}
+
+				hw, err := net.ParseMAC(nd.HardwareAddress)
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Hardware address '%s' is invalid on netdev '%s'", nd.HardwareAddress, nd.Name)
+				}
+				if err := q.AttachTap(nd.Name, t.Name(), hw); err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach tap: err='%s'", err.Error())
+				}
+
+				// Cloudinit settings
+				eth[i] = &configdrive.CloudConfigEthernet{
+					MacAddress: hw,
+				}
+
+				if nd.Ipv4AddressCidr != "" {
+					ip := netutil.ParseCIDR(nd.Ipv4AddressCidr)
+					if ip == nil {
+						return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "Set valid ipv4_address_cidr: value='%s'", nd.Ipv4AddressCidr)
+					}
+					nameservers := make([]net.IP, len(nd.Nameservers))
+					for i, n := range nd.Nameservers {
+						nameservers[i] = net.ParseIP(n)
+					}
+
+					eth[i].Address4 = ip
+					eth[i].Gateway4 = net.ParseIP(nd.Ipv4Gateway)
+					eth[i].NameServers = nameservers
+
+					// Gateway settings
+					if nd.Ipv4Gateway != "" {
+						mask := ip.SubnetMaskBits()
+						gatewayIP := fmt.Sprintf("%s/%d", nd.Ipv4Gateway, mask)
+						if err := b.SetAddress(gatewayIP); err != nil {
+							return nil, grpcutil.WrapGrpcErrorf(codes.Internal, errors.Wrapf(err, "Failed to set gateway IP to bridge: value=%s", gatewayIP).Error())
+						}
+					}
+				}
+			}
+		}
+
+		{
+			parsedKeys := make([]ssh.PublicKey, len(req.SshAuthorizedKeys))
+			for i, k := range req.SshAuthorizedKeys {
+				parsedKeys[i], _, _, _, err = ssh.ParseAuthorizedKey([]byte(k))
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "ssh_authorized_keys is invalid: value='%s', err='%s'", k, err.Error())
+				}
 			}
 
-			i, err := img.OpenQemuImg(u.Path)
+			c := configdrive.StructConfig(req.LoginUsername, req.Name, parsedKeys, eth)
+			p, err := c.Generate(wd)
 			if err != nil {
-				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to open qemu image: err='%s'", err.Error())
+				return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to generate cloudinit configdrive:  err='%s'", err.Error())
 			}
+			req.Blockdevs = append(req.Blockdevs, &BlockDev{
+				Name: "configdrive",
+				Url: (&url.URL{
+					Scheme: "file",
+					Path:   p,
+				}).String(),
+				BootIndex: 50, // MEMO: 適当
+			})
+		}
 
-			// この条件は雑
-			if i.Info.Format == "raw" {
-				if bd.BootIndex < 3 {
-					if err := q.AttachISO(bd.Name, u, uint(bd.BootIndex)); err != nil {
-						return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach iso '%s': err='%s'", u.Path, err.Error())
+		{
+			for _, bd := range req.Blockdevs {
+				u, err := url.Parse(bd.Url)
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.InvalidArgument, "url '%s' is invalid url: '%s'", bd.Url, err.Error())
+				}
+
+				i, err := img.OpenQemuImg(u.Path)
+				if err != nil {
+					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to open qemu image: err='%s'", err.Error())
+				}
+
+				// この条件は雑
+				if i.Info.Format == "raw" {
+					if bd.BootIndex < 3 {
+						if err := q.AttachISO(bd.Name, u, uint(bd.BootIndex)); err != nil {
+							return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach iso '%s': err='%s'", u.Path, err.Error())
+						}
+					} else {
+						if err := q.AttachRaw(bd.Name, u, uint(bd.BootIndex)); err != nil {
+							return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach raw '%s': err='%s'", u.Path, err.Error())
+						}
 					}
 				} else {
-					if err := q.AttachRaw(bd.Name, u, uint(bd.BootIndex)); err != nil {
-						return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach raw '%s': err='%s'", u.Path, err.Error())
+					if err := q.AttachQcow2(bd.Name, u, uint(bd.BootIndex)); err != nil {
+						return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach image '%s': err='%s'", u.String(), err.Error())
 					}
-				}
-			} else {
-				if err := q.AttachQcow2(bd.Name, u, uint(bd.BootIndex)); err != nil {
-					return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to attach image '%s': err='%s'", u.String(), err.Error())
 				}
 			}
 		}
@@ -277,6 +276,8 @@ func (a VirtualMachineAgent) DeleteVirtualMachine(ctx context.Context, req *Dele
 	if err != nil {
 		return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to open qemu process: %s", err.Error())
 	}
+	defer q.Close()
+
 	if q.IsRunning() {
 		if err := q.Delete(); err != nil {
 			return nil, grpcutil.WrapGrpcErrorf(codes.Internal, "Failed to delete qemu: %s", err.Error())
