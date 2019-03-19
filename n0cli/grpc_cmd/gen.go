a22da38b4d39b159ea8cb20c1861eafc395e50ec
h-otter <h-otter@outlook.jp>
2019-03-19 17:41:07 +0900

    hidden flags used as args

--- n0cli/grpc_cmd/gen.go
+++ n0cli/grpc_cmd/gen.go
@@ -17,7 +17,7 @@ func ParseJsonTag(tag string) string {
 }
 
 // "name" is standard field, so get by args
-func GenerateFlags(targetGRPC interface{}) []cli.Flag {
+func GenerateFlags(targetGRPC interface{}, argsKeys []string) []cli.Flag {
 	t := reflect.TypeOf(targetGRPC).In(2).Elem()
 	flags := []cli.Flag{}
 
@@ -29,25 +29,32 @@ func GenerateFlags(targetGRPC interface{}) []cli.Flag {
 			continue
 		}
 
+		hidden := false
+		for _, a := range argsKeys {
+			if a == tag {
+				hidden = true
+			}
+		}
+
 		switch field.Type.Kind() {
 		case reflect.String:
-			flags = append(flags, cli.StringFlag{Name: tag})
+			flags = append(flags, cli.StringFlag{Name: tag, Hidden: hidden})
 
 		case reflect.Bool:
-			flags = append(flags, cli.BoolFlag{Name: tag})
+			flags = append(flags, cli.BoolFlag{Name: tag, Hidden: hidden})
 
 		case reflect.Int64:
-			flags = append(flags, cli.Int64Flag{Name: tag})
+			flags = append(flags, cli.Int64Flag{Name: tag, Hidden: hidden})
 		case reflect.Int32:
-			flags = append(flags, cli.IntFlag{Name: tag})
+			flags = append(flags, cli.IntFlag{Name: tag, Hidden: hidden})
 		case reflect.Uint64:
-			flags = append(flags, cli.Uint64Flag{Name: tag})
+			flags = append(flags, cli.Uint64Flag{Name: tag, Hidden: hidden})
 		case reflect.Uint32:
-			flags = append(flags, cli.UintFlag{Name: tag})
+			flags = append(flags, cli.UintFlag{Name: tag, Hidden: hidden})
 
 		case reflect.Slice:
 			// []string, []structに対応
-			flags = append(flags, cli.StringSliceFlag{Name: tag})
+			flags = append(flags, cli.StringSliceFlag{Name: tag, Hidden: hidden})
 
 			// TODO: []int など
 			// if field.Type == reflect.TypeOf([]string{}) {
@@ -56,8 +63,9 @@ func GenerateFlags(targetGRPC interface{}) []cli.Flag {
 		case reflect.Map:
 			// --map=key:value のような使い方を想定
 			flags = append(flags, cli.StringSliceFlag{
-				Name:  tag,
-				Usage: "set like --option=[key]:[value]",
+				Name:   tag,
+				Usage:  "set like --option=[key]:[value]",
+				Hidden: hidden,
 			})
 		}
 	}
