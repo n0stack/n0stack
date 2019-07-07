69221cf34bcf706b7cbb42506fc883d244f1940a
h-otter <h-otter@outlook.jp>
2019-07-07 20:20:12 +0900

    delete AddPackage method

--- n0core/pkg/api/iam/user/generate_api_test.go
+++ n0core/pkg/api/iam/user/generate_api_test.go
@@ -6,12 +6,22 @@ import (
 	"github.com/n0stack/n0stack/n0core/pkg/util/generator"
 )
 
-// const stringOneRun = `func (i %[2]s)
+// Arguments to format are:
+//	[1]: package
+const ImportedTemplate = `import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/n0stack/n0stack/n0core/pkg/datastore"
+	grpcutil "github.com/n0stack/n0stack/n0core/pkg/util/grpc"
+	p%[1]s "github.com/n0stack/n0stack/n0proto.go/%[1]s/v0"
+	"google.golang.org/grpc/codes"
+)`
 
 // Arguments to format are:
 //	[1]: package
 //	[2]: resource name
-const ListUsersTemplate = `func List%[2]ss(ctx context.Context, req *p%[1]s.ListUsersRequest, ds datastore.Datastore) (*p%[1]s.List%[2]ssResponse, error) {
+const ListTemplate = `func List%[2]ss(ctx context.Context, req *p%[1]s.ListUsersRequest, ds datastore.Datastore) (*p%[1]s.List%[2]ssResponse, error) {
 	res := &p%[1]s.List%[2]ssResponse{}
 	f := func(s int) []proto.Message {
 		res.%[2]ss = make([]*p%[1]s.%[2]s, s)
@@ -40,7 +50,7 @@ const ListUsersTemplate = `func List%[2]ss(ctx context.Context, req *p%[1]s.List
 // Arguments to format are:
 //	[1]: package
 //	[2]: resource name
-const GetUserTemplate = `func Get%[2]s(ctx context.Context, req *p%[1]s.Get%[2]sRequest, ds datastore.Datastore) (*p%[1]s.%[2]s, error) {
+const GetTemplate = `func Get%[2]s(ctx context.Context, req *p%[1]s.Get%[2]sRequest, ds datastore.Datastore) (*p%[1]s.%[2]s, error) {
 	resourse := &p%[1]s.%[2]s{}
 	if err := ds.Get(req.Name, resourse); err != nil {
 		if datastore.IsNotFound(err) {
@@ -53,38 +63,25 @@ const GetUserTemplate = `func Get%[2]s(ctx context.Context, req *p%[1]s.Get%[2]s
 	return resourse, nil
 }`
 
-func GenerateListAPI(gen *generator.GoCodeGenerator, service, resource string) {
-	gen.AddPackage("", "context")
-	gen.AddPackage("", "github.com/golang/protobuf/proto")
-	gen.AddPackage("", "github.com/n0stack/n0stack/n0core/pkg/datastore")
-	gen.AddPackage("grpcutil", "github.com/n0stack/n0stack/n0core/pkg/util/grpc")
-	gen.AddPackage("piam", "github.com/n0stack/n0stack/n0proto.go/iam/v0")
-	gen.AddPackage("", "google.golang.org/grpc/codes")
-
-	gen.Printf(ListUsersTemplate, service, resource)
+func GenerateTemplateAPI(gen *generator.GoCodeGenerator, service, resource string) {
+	gen.Printf(ImportedTemplate, service)
 	gen.Printf("\n")
 	gen.Printf("\n")
-}
 
-func GenerateGetAPI(gen *generator.GoCodeGenerator, service, resource string) {
-	gen.AddPackage("", "context")
-	gen.AddPackage("", "github.com/golang/protobuf/proto")
-	gen.AddPackage("", "github.com/n0stack/n0stack/n0core/pkg/datastore")
-	gen.AddPackage("grpcutil", "github.com/n0stack/n0stack/n0core/pkg/util/grpc")
-	gen.AddPackage("piam", "github.com/n0stack/n0stack/n0proto.go/iam/v0")
-	gen.AddPackage("", "google.golang.org/grpc/codes")
+	gen.Printf(ListTemplate, service, resource)
+	gen.Printf("\n")
+	gen.Printf("\n")
 
-	gen.Printf(GetUserTemplate, service, resource)
+	gen.Printf(GetTemplate, service, resource)
 	gen.Printf("\n")
 	gen.Printf("\n")
 }
 
 func TestGenerate(t *testing.T) {
 	g := generator.NewGoCodeGenerator("template_api", "userapi")
-	GenerateListAPI(g, "iam", "User")
-	GenerateGetAPI(g, "iam", "User")
+	GenerateTemplateAPI(g, "iam", "User")
 
-	if err := g.WriteWithTemplateFileName(); err != nil {
+	if err := g.WriteAsTemplateFileName(); err != nil {
 		t.Errorf("err=%s", err.Error())
 	}
 }
--- n0core/pkg/util/generator/generator_test.go
+++ n0core/pkg/util/generator/generator_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestGoCodeGenerator(t *testing.T) {
 	g := NewGoCodeGenerator("test_generator", "generator")
-	if err := g.WriteWithTemplateFileName(); err != nil {
+	if err := g.WriteAsTemplateFileName(); err != nil {
 		t.Errorf("err=%s, src=\n%s", err.Error(), g.code.String())
 	}
-	defer os.Remove("test_generator.go")
+	defer os.Remove("test_generator.generated.go")
 }
